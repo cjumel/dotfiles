e5eb92fc789a1c60f5a29b0189e76116a467ce79
clementjumel <clement.jumel@gmail.com>
2023-05-04 15:54:04 +0200

    :wrench: Update aliases

--- .oh-my-zsh/custom/core.zsh
+++ .oh-my-zsh/custom/core.zsh
@@ -9,32 +9,22 @@ alias alup='
     source ~/.zshrc
 '
 
-function bind(){ ssh -N -f -L localhost:$2:localhost:$3 $1 }
-
 alias clr='clear'
-
-function copy-to(){ rsync -r $2 $1:$3 }  # $1: user@host; $2: local path; $3: remote path
-function copy-from(){ rsync -r $1:$2 $3 }  # $1: user@host; $2: remote path; $3: local path
 
 alias duh='du -d 1 -h 2>/dev/null'
 alias duhbig='du -d 1 -h -t1G 2>/dev/null'
 
-alias xt='exit'
-
 function pc(){ open -na "PyCharm.app" --args "$1" }
-alias pcdot='open -na "PyCharm.app" --args ~/dotfiles/'
 
 alias sc='screen'
-alias scls='screen -ls'  # List current sessions
 
-alias src='source'
-
-alias tch='touch'
+alias so='source'
 
 alias tm='tmux'
-alias tma='tmux attach'
-alias tmls='tmux ls'
 alias tmn='tmux new'
 
+alias to='touch'
+
 alias vi='nvim'
-alias vidot='nvim ~/dotfiles'
+
+alias xt='exit'
