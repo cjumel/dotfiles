102fba3db57236b289e40b57ed3a7ae1fec34d63
clementjumel <clement.jumel@gmail.com>
2023-05-06 12:00:01 +0200

    :sparkles: Enable git icons in nvim

---
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+  renderer = {
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,7 +1,13 @@
+local overrides = require("custom.configs.overrides")
+
 local plugins = {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
-  }
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
 }
 return plugins
