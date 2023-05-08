2ad2219b2e4c2314e39f18a20ab8e35fc61d10c4
clementjumel <clement.jumel@gmail.com>
2023-05-08 12:31:56 +0200

    :recycle: Simplify core shortcuts

--- .oh-my-zsh/custom/core.zsh
+++ .oh-my-zsh/custom/core.zsh
@@ -1,13 +1,5 @@
 function al(){ alias | grep "^$1" }  # List aliases starting with the input string
 function alf(){ functions | grep "^$1" }  # List functions starting with the input string
-alias alup='
-    cd ~/dotfiles
-    git add .oh-my-zsh/custom
-    git commit -m ":wrench: Update aliases"
-    git push
-    1
-    source ~/.zshrc
-'
 
 alias clr='clear'
 
@@ -17,6 +9,7 @@ alias duhbig='du -d 1 -h -t1G 2>/dev/null'
 alias sc='screen'
 
 alias so='source'
+alias sozsh='source ~/.zshrc'
 
 alias to='touch'
 
