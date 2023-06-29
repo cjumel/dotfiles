4ac27b5328c8fcd2f0ac31cb4f32c3e1a7c9806a
clementjumel <clement.jumel@gmail.com>
2023-06-29 22:23:57 +0200

    💡 (nvim): separate mappings between plugins

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,10 +1,12 @@
 local M = {}
 M.general = {
   n = {
+    -- vim-tmux-navigator
     ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
     ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
     ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+    -- vim-fugitive
     ["<leader>gs"] = { "<cmd> tab Git<CR>", "Git status" },
   }
 }
