ba79395b6b20698ea3639bac38f4a3664b5963ef
clementjumel <clement.jumel@gmail.com>
2023-08-06 11:08:15 +0200

    ✨ (nvim): LSP server for Python

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -27,6 +27,21 @@ local plugins = {
       "HopLineBC",
     },
   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
 }
 
 return plugins
