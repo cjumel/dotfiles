c71116562e8e16e45cb1a6ff936694bf65f3fc80
clementjumel <clement.jumel@gmail.com>
2023-08-06 11:51:07 +0200

    ✨ (nvim): shortcuts to write to-do comments

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -20,6 +20,10 @@ M.general = {
     ["<leader>b"] = { "<cmd> HopWordCurrentLineBC <CR>", "Jump to any word before the cursor" },
     ["<leader>j"] = { "<cmd> HopLineAC <CR>", "Jump to any line after the cursor" },
     ["<leader>k"] = { "<cmd> HopLineBC <CR>", "Jump to any line before the cursor" },
+    -- To-do comments
+    ["to"] = {"O# todo<ESC>", "Write a to-do comment above the line cursor"},
+    ["ti"] = {"I# todo <ESC>", "Write a to-do comment at the beginning of the line cursor"},
+    ["ta"] = {"A# todo<ESC>", "Write a to-do comment at the end of the line cursor"},
   },
   v = {
     -- hop.nvim
