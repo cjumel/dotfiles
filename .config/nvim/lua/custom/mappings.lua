5b5b886ad2bd90c141ccf9319405279b86c4fbc3
clementjumel <clement.jumel@gmail.com>
2023-08-07 10:15:24 +0200

    ✨ (nvim): leader + e/ge mapping with hop

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -7,6 +7,8 @@ M.general = {
 		["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "Window right" },
 		["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "Window down" },
 		["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "Window up" },
+		-- nvim-tree
+		["<leader>n"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvim-tree" },
 		-- vim-fugitive
 		["<leader>gs"] = {
 			"<cmd> tab Git <CR> \
@@ -15,17 +17,51 @@ M.general = {
 			"Git status",
 		},
 		-- hop.nvim
-		["s"] = { "<cmd> HopChar2 <CR>", "Jump to any combinaison of 2 keys" },
-		["<leader>w"] = { "<cmd> HopWordCurrentLineAC <CR>", "Jump to any word after the cursor" },
-		["<leader>b"] = { "<cmd> HopWordCurrentLineBC <CR>", "Jump to any word before the cursor" },
+		["s"] = {
+			"<cmd> HopChar2 <CR>",
+			"Jump to any combinaison of 2 keys",
+		},
+		["<leader>w"] = {
+			"<cmd> HopWordCurrentLineAC <CR>",
+			"Jump to any word beginning after the cursor",
+		},
+		["<leader>b"] = {
+			"<cmd> HopWordCurrentLineBC <CR>",
+			"Jump to any word beginning before the cursor",
+		},
+		["<leader>e"] = {
+			"<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true }) <CR>",
+			"Jump to any word end after the cursor",
+		},
+		["<leader>ge"] = {
+			"<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true }) <CR>",
+			"Jump to any word end before the cursor",
+		},
 		["<leader>j"] = { "<cmd> HopLineAC <CR>", "Jump to any line after the cursor" },
 		["<leader>k"] = { "<cmd> HopLineBC <CR>", "Jump to any line before the cursor" },
 	},
 	v = {
 		-- hop.nvim
-		["s"] = { "<cmd> HopChar2 <CR>", "Jump to any combinaison of 2 keys" },
-		["<leader>w"] = { "<cmd> HopWordCurrentLineAC <CR>", "Jump to any word after the cursor" },
-		["<leader>b"] = { "<cmd> HopWordCurrentLineBC <CR>", "Jump to any word before the cursor" },
+		["s"] = {
+			"<cmd> HopChar2 <CR>",
+			"Jump to any combinaison of 2 keys",
+		},
+		["<leader>w"] = {
+			"<cmd> HopWordCurrentLineAC <CR>",
+			"Jump to any word beginning after the cursor",
+		},
+		["<leader>b"] = {
+			"<cmd> HopWordCurrentLineBC <CR>",
+			"Jump to any word beginning before the cursor",
+		},
+		["<leader>e"] = {
+			"<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true }) <CR>",
+			"Jump to any word end after the cursor",
+		},
+		["<leader>ge"] = {
+			"<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true }) <CR>",
+			"Jump to any word end before the cursor",
+		},
 		["<leader>j"] = { "<cmd> HopLineAC <CR>", "Jump to any line after the cursor" },
 		["<leader>k"] = { "<cmd> HopLineBC <CR>", "Jump to any line before the cursor" },
 	},
