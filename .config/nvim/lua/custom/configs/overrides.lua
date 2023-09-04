8c08b9b329370a3f0edfb3438b1db58003e35925
clementjumel <clement.jumel@gmail.com>
2023-09-04 21:56:30 +0200

    ⚡️ (nvim): improve oil.nvim keymaps

--- .config/nvim/lua/custom/configs/overrides.lua
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -30,6 +30,19 @@ M.nvimtree = {
 
 M.oil = {
 	default_file_explorer = false,
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-v>"] = "actions.select_vsplit",
+		["<C-s>"] = "actions.select_split",
+		["<C-t>"] = "actions.select_tab",
+		["<C-p>"] = "actions.preview",
+		["q"] = "actions.close",
+		["-"] = "actions.parent",
+		["_"] = "actions.open_cwd",
+		["H"] = "actions.toggle_hidden",
+	},
+	use_default_keymaps = false,
 	view_options = {
 		show_hidden = true,
 		is_always_hidden = function(name, _)
