f6d5fc5e52caa551ef2e5f2dc40d153cc9f110e6
clementjumel <clement.jumel@gmail.com>
2023-09-05 09:22:25 +0200

    ⚡️ (nvim): improve nvimtree keymaps

--- .config/nvim/lua/custom/configs/overrides.lua
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -25,6 +25,20 @@ M.nvimtree = {
 			"^.null-ls_",
 		},
 	},
+	on_attach = function(bufnr)
+		local api = require("nvim-tree.api")
+
+		local function opts(desc)
+			return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+		end
+
+		-- default mappings
+		api.config.mappings.default_on_attach(bufnr)
+
+		-- custom mappings
+		vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+		vim.keymap.set("n", "<C-s>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+	end,
 }
 
 M.oil = {
