e124054ac6601a94dfe9dd1728160b5acf6868ad
clementjumel <clement.jumel@gmail.com>
2023-09-05 14:55:30 +0200

    ⚡️ (nvim): use = keymap in nvimtree

--- .config/nvim/lua/custom/configs/overrides.lua
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -50,6 +50,7 @@ M.nvimtree = {
 		api.config.mappings.default_on_attach(bufnr)
 
 		-- custom mappings
+		vim.keymap.set("n", "=", api.node.open.edit, opts("Open"))
 		vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
 		vim.keymap.set("n", "<C-s>", api.node.open.horizontal, opts("Open: Horizontal Split"))
 	end,
