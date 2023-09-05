7ee34a3871f8760448e99556ae2da6c66bf2d395
clementjumel <clement.jumel@gmail.com>
2023-09-05 10:16:08 +0200

    ♻️ (nvim): re-organize mappings

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,23 +2,54 @@ local M = {}
 
 M.general = {
 	n = {
-		-- vim-tmux-navigator
-		["<C-h>"] = { "<cmd> TmuxNavigateLeft <CR>", "Window left" },
-		["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "Window right" },
-		["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "Window down" },
-		["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "Window up" },
-		-- splits
+
+		-- Core keymaps
+
 		["<leader>v"] = { "<cmd> vsplit <CR>", "Vertical split" },
 		["<leader>s"] = { "<cmd> split <CR>", "Horizontal split" },
+
+		-- Keymaps overrides for NvChad default plugins
+		-- See ~/.config/nvim/lua/core/mappings.lua for NvChad mappings definition
+
+		-- nvterm
+		["<leader>te"] = {
+			function()
+				require("nvterm.terminal").toggle("horizontal")
+			end,
+			"Toggle horizontal terminal",
+		},
+
+		-- Comment.nvim
+		["gtc"] = {
+			function()
+				vim.api.nvim_command("normal! ITODO: ")
+				require("Comment.api").toggle.linewise.current()
+				vim.api.nvim_command("normal! 3l")
+			end,
+			"Add a TODO comment in the current line",
+		},
+		["gtO"] = {
+			-- FIXME: this is not working when the current line is a comment
+			function()
+				vim.api.nvim_command("normal! OTODO: ")
+				require("Comment.api").toggle.linewise()
+				vim.api.nvim_command("normal! 3l")
+			end,
+			"Add a TODO comment in the line above the cursor",
+		},
+		["gto"] = {
+			-- FIXME: this is not working when the current line is a comment
+			function()
+				vim.api.nvim_command("normal! oTODO: ")
+				require("Comment.api").toggle.linewise.current()
+				vim.api.nvim_command("normal! 3l")
+			end,
+			"Add a TODO comment in the line below the cursor",
+		},
+
 		-- nvim-tree
 		["<leader>n"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvim-tree" },
-		-- oil.nvim
-		["-"] = {
-			function()
-				require("oil").open()
-			end,
-			"Open parent directory with oil.nvim",
-		},
+
 		-- telescope.nvim
 		["<leader>fb"] = {
 			function()
@@ -88,119 +119,23 @@ M.general = {
 			end,
 			"Find the word or selection under the cursor",
 		},
-		-- nvterm
-		["<leader>te"] = {
+
+		-- Custom plugins
+
+		-- vim-tmux-navigator
+		["<C-h>"] = { "<cmd> TmuxNavigateLeft <CR>", "Window left" },
+		["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "Window right" },
+		["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "Window down" },
+		["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "Window up" },
+
+		-- oil.nvim
+		["-"] = {
 			function()
-				require("nvterm.terminal").toggle("horizontal")
+				require("oil").open()
 			end,
-			"Toggle horizontal terminal",
+			"Open parent directory with oil.nvim",
 		},
-		-- vim-fugitive
-		["<leader>gs"] = {
-			"<cmd> tab Git <CR> \
-      <cmd> highlight diffAdded guifg=#abe9b3 <CR> \
-      <cmd> highlight diffRemoved guifg=#f38ba8 <CR>",
-			"Git status",
-		},
-		-- hop.nvim
-		["s"] = {
-			function()
-				require("hop").hint_char2()
-			end,
-			"Jump to a combinaison of 2 keys",
-		},
-		["f"] = {
-			function()
-				require("hop").hint_char1({
-					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-					current_line_only = true,
-				})
-			end,
-			"Jump to a following key inline",
-		},
-		["F"] = {
-			function()
-				require("hop").hint_char1({
-					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-					current_line_only = true,
-				})
-			end,
-			"Jump to a preceding key inline",
-		},
-		["t"] = {
-			function()
-				require("hop").hint_char1({
-					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-					current_line_only = true,
-					hint_offset = -1,
-				})
-			end,
-			"Jump to before a following key inline",
-		},
-		["T"] = {
-			function()
-				require("hop").hint_char1({
-					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-					current_line_only = true,
-					hint_offset = 1,
-				})
-			end,
-			"Jump to after a preceding key inline",
-		},
-		["<leader>w"] = {
-			function()
-				require("hop").hint_words({
-					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-					current_line_only = true,
-				})
-			end,
-			"Jump to a following word beginning inline",
-		},
-		["<leader>b"] = {
-			function()
-				require("hop").hint_words({
-					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-					current_line_only = true,
-				})
-			end,
-			"Jump to a preceding word beginning inline",
-		},
-		["<leader>e"] = {
-			function()
-				require("hop").hint_words({
-					hint_position = require("hop.hint").HintPosition.END,
-					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-					current_line_only = true,
-				})
-			end,
-			"Jump to a following word end inline",
-		},
-		["<leader>ge"] = {
-			function()
-				require("hop").hint_words({
-					hint_position = require("hop.hint").HintPosition.END,
-					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-					current_line_only = true,
-				})
-			end,
-			"Jump to a preceding word end inline",
-		},
-		["<leader>j"] = {
-			function()
-				require("hop").hint_lines({
-					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-				})
-			end,
-			"Jump to a following line",
-		},
-		["<leader>k"] = {
-			function()
-				require("hop").hint_lines({
-					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-				})
-			end,
-			"Jump to a preceding line",
-		},
+
 		-- harpoon
 		["<leader>ha"] = {
 			function()
@@ -256,40 +191,124 @@ M.general = {
 			end,
 			"Go to previous harpoon file",
 		},
-		-- Comment.nvim
-		["gtc"] = {
-			function()
-				vim.api.nvim_command("normal! ITODO: ")
-				require("Comment.api").toggle.linewise.current()
-				vim.api.nvim_command("normal! 3l")
-			end,
-			"Add a TODO comment in the current line",
-		},
-		["gtO"] = {
-			-- FIXME: this is not working when the current line is a comment
-			function()
-				vim.api.nvim_command("normal! OTODO: ")
-				require("Comment.api").toggle.linewise()
-				vim.api.nvim_command("normal! 3l")
-			end,
-			"Add a TODO comment in the line above the cursor",
-		},
-		["gto"] = {
-			-- FIXME: this is not working when the current line is a comment
-			function()
-				vim.api.nvim_command("normal! oTODO: ")
-				require("Comment.api").toggle.linewise.current()
-				vim.api.nvim_command("normal! 3l")
-			end,
-			"Add a TODO comment in the line below the cursor",
-		},
+
 		-- todo-comments.nvim
 		["<leader>td"] = {
 			"<cmd> TodoQuickFix <CR>",
 			"Display the TODO comments in a quickfix tab",
 		},
+
+		-- hop.nvim
+		["s"] = {
+			function()
+				require("hop").hint_char2()
+			end,
+			"Jump to a combinaison of 2 keys",
+		},
+		["f"] = {
+			function()
+				require("hop").hint_char1({
+					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+					current_line_only = true,
+				})
+			end,
+			"Jump to a following key inline",
+		},
+		["F"] = {
+			function()
+				require("hop").hint_char1({
+					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+					current_line_only = true,
+				})
+			end,
+			"Jump to a preceding key inline",
+		},
+		["t"] = {
+			function()
+				require("hop").hint_char1({
+					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+					current_line_only = true,
+					hint_offset = -1,
+				})
+			end,
+			"Jump to before a following key inline",
+		},
+		["T"] = {
+			function()
+				require("hop").hint_char1({
+					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+					current_line_only = true,
+					hint_offset = 1,
+				})
+			end,
+			"Jump to after a preceding key inline",
+		},
+		["<leader>w"] = {
+			function()
+				require("hop").hint_words({
+					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+					current_line_only = true,
+				})
+			end,
+			"Jump to a following word beginning inline",
+		},
+		["<leader>b"] = {
+			function()
+				require("hop").hint_words({
+					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+					current_line_only = true,
+				})
+			end,
+			"Jump to a preceding word beginning inline",
+		},
+		["<leader>e"] = {
+			function()
+				require("hop").hint_words({
+					hint_position = require("hop.hint").HintPosition.END,
+					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+					current_line_only = true,
+				})
+			end,
+			"Jump to a following word end inline",
+		},
+		["<leader>ge"] = {
+			function()
+				require("hop").hint_words({
+					hint_position = require("hop.hint").HintPosition.END,
+					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+					current_line_only = true,
+				})
+			end,
+			"Jump to a preceding word end inline",
+		},
+		["<leader>j"] = {
+			function()
+				require("hop").hint_lines({
+					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+				})
+			end,
+			"Jump to a following line",
+		},
+		["<leader>k"] = {
+			function()
+				require("hop").hint_lines({
+					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+				})
+			end,
+			"Jump to a preceding line",
+		},
+
+		-- vim-fugitive
+		["<leader>gs"] = {
+			"<cmd> tab Git <CR> \
+      <cmd> highlight diffAdded guifg=#abe9b3 <CR> \
+      <cmd> highlight diffRemoved guifg=#f38ba8 <CR>",
+			"Git status",
+		},
 	},
+
 	x = {
+
 		-- hop.nvim
 		["s"] = {
 			function()
@@ -390,6 +409,7 @@ M.general = {
 			"Jump to a preceding line",
 		},
 	},
+
 	o = {
 		-- hop.nvim
 		["s"] = {
@@ -491,7 +511,9 @@ M.general = {
 			"Jump to a preceding line",
 		},
 	},
+
 	t = {
+
 		-- terminal mode
 		["<leader>x"] = {
 			function()
@@ -504,24 +526,19 @@ M.general = {
 
 M.disabled = {
 	n = {
-		-- nvim-tree
-		["<C-n>"] = "",
-		-- telescope
 		["<leader>cm"] = "",
 		["<leader>fw"] = "",
 		["<leader>gt"] = "",
-		-- nvterm
-		["<leader>v"] = "",
 		["<leader>h"] = "",
-		-- Makes <leader>w slow
+		["<leader>v"] = "",
 		["<leader>wa"] = "",
 		["<leader>wk"] = "",
 		["<leader>wK"] = "",
 		["<leader>wl"] = "",
 		["<leader>wr"] = "",
+		["<C-n>"] = "",
 	},
 	v = {
-		-- Makes <leader>w slow
 		["<leader>wa"] = "",
 		["<leader>wk"] = "",
 		["<leader>wK"] = "",
