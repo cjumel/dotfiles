303379db692187a128102699e8bbca37934a2738
clementjumel <clement.jumel@gmail.com>
2023-09-14 13:38:08 +0200

    ✨ (oh-my-zsh): new pytest-related aliases

--- .oh-my-zsh/custom/python.zsh
+++ .oh-my-zsh/custom/python.zsh
@@ -3,11 +3,15 @@ alias jnb='jupyter notebook'
 alias mkd='mkdocs serve'
 
 alias pte='pytest'
-alias ptex='pytest --exitfirst'
+alias ptedb='pytest --pdb'
 alias ptef='pytest -m "not slow"'
+alias pteff='pytest --failed-first'
 alias ptefx='pytest -m "not slow" --exitfirst'
+alias ptelf='pytest --last-failed'
+alias ptenf='pytest --new-first'
 alias ptes='pytest -m "slow"'
 alias ptesx='pytest -m "slow" --exitfirst'
+alias ptex='pytest --exitfirst'
 
 alias py='python'
 
