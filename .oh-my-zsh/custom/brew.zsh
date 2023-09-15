403e1290b8b155e635b5ca5dd8661e5fef1cd3cd
clementjumel <clement.jumel@gmail.com>
2023-09-15 10:54:47 +0200

    ✨ (oh-my-zsh): brew pin/unpin aliases

--- .oh-my-zsh/custom/brew.zsh
+++ .oh-my-zsh/custom/brew.zsh
@@ -1,12 +1,24 @@
 alias bb='brew bundle'
 alias bbd='brew bundle dump'  # Write the installed brew formulae & casks in a Brewfile
+
 alias bc='brew cleanup'
+
 alias bi='brew install'
 alias bic='brew install --cask'
+
 alias bif='brew info'
+
 alias bl='brew list'
+
 alias bo='brew outdated'
+
+alias bp='brew pin'
+
 alias bud='brew update'  # Update homebrew itself
+
 alias bug='brew upgrade'  # Update all outdated formulae and casks
 alias bugg='brew upgrade --greedy'  # Include casks with auto-updates or version "latest"
+
 alias bui='brew uninstall'
+
+alias bup='brew unpin'
