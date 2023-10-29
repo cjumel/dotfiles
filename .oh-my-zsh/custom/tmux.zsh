5629dd523c6bba4144e3735301d4f252174a74a2
clementjumel <clement.jumel@gmail.com>
2023-10-29 09:33:26 +0100

    ♻️ (oh-my-zsh): re-organize tmux aliases

--- .oh-my-zsh/custom/tmux.zsh
+++ .oh-my-zsh/custom/tmux.zsh
@@ -1,9 +1,14 @@
-alias tm='tmux'
+alias tm='tmux'  # Equivalent to 'tmux new'
+alias tms='tmux new -s'  # New session with name
+
 alias tma='tmux attach'  # Attach to last session
 alias tmas='tmux attach -t'  # Attach to session with name
+
 alias tmd='tmux detach'
+
 alias tmk='tmux kill-session -t'  # Kill session with name
 alias tmK='tmux kill-server'
+
 alias tml='tmux ls'
+
 alias tmr='tmux rename'
-alias tms='tmux new -s'  # New session with name
