9edc39d9a7c4d79defed9902e1013bd72a46a20e
clementjumel <clement.jumel@gmail.com>
2023-10-29 12:54:14 +0100

    ✨ (oh-my-zsh): quick python file alias

--- .oh-my-zsh/custom/nvim.zsh
+++ .oh-my-zsh/custom/nvim.zsh
@@ -2,6 +2,7 @@ alias vi='nvim'
 alias vip='poetry run nvim'
 
 alias qn='nvim ./notes.md'
+alias qp='nvim ./temp.py'
 
 alias vic='rm -rf ~/.local/share/nvim/' # Clear nvim cache
 function vil(){  # Link nvim config to the input **absolute** path
