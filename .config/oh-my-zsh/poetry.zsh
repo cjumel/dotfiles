e3e2a90791dd53445c8f4d1bb034df147b487b58
clementjumel <clement.jumel@gmail.com>
2023-11-07 16:03:26 +0100

    ✨ (oh-my-zsh): more aliases for poetry

--- .config/oh-my-zsh/poetry.zsh
+++ .config/oh-my-zsh/poetry.zsh
@@ -1,17 +1,29 @@
 alias poa='poetry add'
+
 alias popb='poetry publish'
 alias popbb='poetry publish --build'
+
 alias popi='poetry pin'
+
 alias poc='poetry cache'
 alias pocc='poetry cache clear --all .'
+
 alias poe='poetry env'
 alias poei='poetry env info'
 alias poel='poetry env list'
 alias poer='poetry env remove'
 alias poeu='poetry env use'
+
 alias poi='poetry install'
-alias poin='poetry init'
+
+alias poii='poetry init'
+
+alias pl='poetry lock' # Lock the dependencies of the pyproject.toml with latest versions but don't install them
+alias pln='poetry lock --no-update' # Lock without updating the dependencies to latest versions
+
 alias por='poetry run'
+
 alias pos='poetry shell'
+
 alias pou='poetry update'
 alias pouv='poetry update -vvv'
