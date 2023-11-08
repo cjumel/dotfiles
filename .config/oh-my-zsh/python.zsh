7c2bfed0d07c936d04498ebc35a1a6b97899c42a
clementjumel <clement.jumel@gmail.com>
2023-11-08 17:49:15 +0100

    ⚡️ (oh-my-zsh): move venv aliases to own files & improve them

--- .config/oh-my-zsh/python.zsh
+++ .config/oh-my-zsh/python.zsh
@@ -14,9 +14,3 @@ alias ptesx='pytest -m "slow" --exitfirst'
 alias ptex='pytest --exitfirst'
 
 alias py='python'
-
-function vea(){ source "$1/bin/activate" }  # Activate an environemnt
-alias veav='source .venv/bin/activate'
-alias vec='python3 -m venv'  # Create a venv environment
-alias vecv='python3 -m venv .venv'
-alias ved='deactivate'  # Deactivate an environment
---
+++ .config/oh-my-zsh/venv.zsh
@@ -0,0 +1,5 @@
+# All these aliases assume we use/create an environment located in .venv/
+alias vec='python -m venv .venv' # Create
+alias vea='source .venv/bin/activate' # Activate
+alias ved='deactivate' # Deactivate
+alias ver='rm -rf .venv' # Remove
