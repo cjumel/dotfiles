3e505f03548cc4a5d9870a2437142d8a6e27a34b
clementjumel <clement.jumel@gmail.com>
2023-11-20 12:06:07 +0100

    ✨ (tmux): add light configuration

--- .config/oh-my-zsh/tmux.zsh
+++ .config/oh-my-zsh/tmux.zsh
@@ -1,4 +1,5 @@
 alias tm='tmux'
+alias tml='tmux -f ~/dotfiles/.config/tmux/tmux_light.conf'  # Use light configuration
 
 alias tma='tmux attach'  # Attach to last session
 alias tmat='tmux attach -t'  # Attach to session with target name
@@ -9,6 +10,8 @@ alias tml='tmux ls'
 
 alias tmn='tmux new'
 alias tmns='tmux new -s'  # New session with name
+alias tmln='tmux -f ~/dotfiles/.config/tmux/tmux_light.conf new'
+alias tmlns='tmux -f ~/dotfiles/.config/tmux/tmux_light.conf new -s'
 
 alias tmk='tmux kill-session'
 alias tmkt='tmux kill-session -t'  # Kill session with target name
@@ -18,4 +21,5 @@ alias tmK='tmux kill-server'
 alias tmr='tmux rename'
 
 alias tms='tmux source'
-alias tmsc='tmux source ~/.config/tmux/tmux.conf'
+alias tmsc='tmux source ~/.config/tmux/tmux.conf'  # Source default configuration
+alias tmsl='tmux source ~/.config/tmux/tmux_light.conf'  # Source light configuration
