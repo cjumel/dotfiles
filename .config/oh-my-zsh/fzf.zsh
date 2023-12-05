cc818f32c9acaf5b0ea4a80b95c88045c391d8c4
clementjumel <clement.jumel@gmail.com>
2023-12-05 17:13:42 +0100

    🔥 (oh-my-zsh): remove unused fzf aliases

--- .config/oh-my-zsh/fzf.zsh
+++ .config/oh-my-zsh/fzf.zsh
@@ -1,23 +1,3 @@
-# Find files and directories fuzzyily with fd and fzf
-# These aliases don't do anything on their own but can be passed as variable in a command or in
-# other aliases, for instance with 'cd $(fdzd)'
-alias fdz='fd . | fzf --layout reverse'
-alias fdzh='fd . --hidden | fzf --layout reverse'
-alias fdzd='(echo "./"; fd . --type d) | fzf --layout reverse' # Extended with the current directory
-alias fdzdh='(echo "./"; fd . --type d --hidden) | fzf --layout reverse' # Extended with the current directory
-alias fdzf='fd . --type f | fzf --layout reverse'
-alias fdzfh='fd . --type f --hidden | fzf --layout reverse'
-
 # Navigation but fuzzily
-alias fz='cd $(fdzd)'
-alias fzh='cd $(fdzdh)'
-
-# Copy but fuzzily
-alias cpf='cp $(fdzf) $(fdzd)'
-alias cpfh='cp $(fdzfh) $(fdzdh)'
-alias cprf='cp -r $(fdzd) $(fdzd)'
-alias cprfh='cp -r $(fdzdh) $(fdzdh)'
-
-# Move but fuzzily
-alias mvf='mv $(fdz) $(fdzd)'
-alias mvfh='mv $(fdzh) $(fdzdh)'
+alias fz='cd $((echo "./"; fd . --type d) | fzf --layout reverse)'
+alias fzh='cd $((echo "./"; fd . --type d --hidden) | fzf --layout reverse)'
