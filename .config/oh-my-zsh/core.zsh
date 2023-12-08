5624b3ca1229e9b5d6a1be50541678291dd59db4
clementjumel <clement.jumel@gmail.com>
2023-12-08 14:16:41 +0100

    ⚡️ (oh-my-zsh): update core aliases

--- .config/oh-my-zsh/core.zsh
+++ .config/oh-my-zsh/core.zsh
@@ -2,9 +2,6 @@ function al(){ alias | grep "^$1" }  # List aliases starting with the input stri
 
 alias cd='HOME=~/Code/ cd'
 
-alias duh='du -d 1 -h 2>/dev/null'  # Human readable disk usage
-alias duhl='du -d 1 -h -t1G 2>/dev/null'  # Human readable disk usage with large files & directories only
-
 function fu(){ functions | grep "^$1" }  # List functions starting with the input string
 
 alias lns='ln -s'
@@ -23,6 +20,8 @@ alias so='source'
 alias soz='source ~/.zshrc'
 
 alias to='touch'
+
+alias uln='unlink'
 
 alias wi='which'
 alias we='whence'
