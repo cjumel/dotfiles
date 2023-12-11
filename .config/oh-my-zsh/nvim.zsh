571f99da100b2ef9988630dc2519dfeb02f0c508
clementjumel <clement.jumel@gmail.com>
2023-12-11 16:16:32 +0100

    ⚡️ (oh-my-zsh): simplify neovim with fuzzy finding aliases

--- .config/oh-my-zsh/nvim.zsh
+++ .config/oh-my-zsh/nvim.zsh
@@ -1,12 +1,8 @@
 alias vi='nvim'
 
-# Open a directory in nvim with fuzzy finding
-alias vid='nvim $((echo "./"; fddd) | fz)'
-alias vidh='nvim $((echo "./"; fdddh) | fz)'
-
-# Open a file in nvim with fuzzy finding
-alias vif='nvim $(fddf| fz)'
-alias vifh='nvim $(fddfh| fz)'
+# Neovim with fuzzy finding
+alias vif='nvim $((echo "./"; fdd) | fz)'
+alias vifh='nvim $((echo "./"; fddh) | fz)'
 
 alias vip='poetry run nvim'
 
