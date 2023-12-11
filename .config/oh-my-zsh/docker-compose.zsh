38dde8628ede95cb63d091c9f16e911af455e0e6
clementjumel <clement.jumel@gmail.com>
2023-12-11 14:24:17 +0100

    ♻️ Separate docker & docker-compose aliases

---
+++ .config/oh-my-zsh/docker-compose.zsh
@@ -0,0 +1,8 @@
+alias dc='docker-compose'
+
+alias dcb='docker-compose build'
+alias dcbn='docker-compose build --no-cache'
+
+alias dcr='docker-compose run'
+
+alias dcu='docker-compose up'
--- .config/oh-my-zsh/docker.zsh
+++ .config/oh-my-zsh/docker.zsh
@@ -1,6 +1,9 @@
 alias dk='docker'
+
 alias dka='open -a "Docker"' # start application
+
 alias dkb='docker build'
+
 # clean docker containers, images, volumes, etc.
 alias dkc='
   echo "docker stop $(docker ps -a -q)..."; docker stop $(docker ps -a -q)
@@ -9,12 +12,9 @@ alias dkc='
   echo "docker image prune --all --force..."; docker image prune --all --force
   echo "docker system prune --all --force..."; docker system prune --all --force
 '
-alias dkp='docker pull'
-alias dkr='docker run'
-alias dks='docker scout'
 
-alias dc='docker-compose'
-alias dcb='docker-compose build'
-alias dcbn='docker-compose build --no-cache'
-alias dcr='docker-compose run'
-alias dcu='docker-compose up'
+alias dkp='docker pull'
+
+alias dkr='docker run'
+
+alias dks='docker scout'
