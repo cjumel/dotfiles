9a7c678ac63cb204bf35e464639ee4d812ad8b86
clementjumel <clement.jumel@gmail.com>
2023-12-11 15:07:59 +0100

    🔥 (tmux): remove light theme config file

--- .config/oh-my-zsh/tmux.zsh
+++ .config/oh-my-zsh/tmux.zsh
@@ -8,9 +8,7 @@ alias tmd='tmux detach'
 alias tml='tmux ls'
 
 alias tmn='tmux new'  # New unnamed session
-alias tmnl='tmux -f ~/dotfiles/.config/tmux/tmux_light.conf new'  # New unnamed session with light theme
 alias tmns='tmux new -s'  # New named sessions
-alias tmnsl='tmux -f ~/dotfiles/.config/tmux/tmux_light.conf new -s'  # New named session with light theme
 
 alias tmk='tmux kill-session'
 alias tmkt='tmux kill-session -t'  # Kill session with target name
@@ -21,4 +19,3 @@ alias tmr='tmux rename'
 
 alias tms='tmux source'
 alias tmsc='tmux source ~/.config/tmux/tmux.conf'  # Source default configuration
-alias tmsl='tmux source ~/dotfiles/.config/tmux/tmux_light.conf'  # Source light configuration
