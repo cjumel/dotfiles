b33c4a5bab28f11c9146dbbae567ba0366c6fff4
clementjumel <clement.jumel@gmail.com>
2023-12-17 21:51:11 +0100

    🔥 (oh-my-zsh): remove navigation aliases

--- .config/oh-my-zsh/ignored/_navigation.zsh
+++
@@ -1,8 +0,0 @@
-# Goes to ~/Code/ by default
-alias cd='HOME=~/Code/ cd'
-
-# Machine-specific aliases
-alias cod='cd ~/Code/'
-alias doc='cd ~/Documents/'
-alias dwl='cd ~/Downloads/'
-alias dsk='cd ~/Desktop/'
