474857fe3f9cabfd1a3bd4505309919328f4ee1a
clementjumel <clement.jumel@gmail.com>
2024-01-12 15:43:25 +0100

    ✨ (oh-my-zsh): few more pytest aliases

--- .config/oh-my-zsh/pytest.zsh
+++ .config/oh-my-zsh/pytest.zsh
@@ -6,7 +6,11 @@ alias ptelf='pytest --last-failed' # Run only the tests that failed last time
 
 alias ptem='pytest -m' # Run tests with a marker
 alias ptemf='pytest -m "not slow"' # Marked as fast
+alias ptemfx='pytest -m "not slow" -x' # Marked as fast and stop on first failure
+alias ptemfxs='pytest -m "not slow" -x -s' # Marked as fast and stop on first failure and show output
 alias ptems='pytest -m "slow"' # Marked as slow
+alias ptemsx='pytest -m "slow" -x' # Marked as slow and stop on first failure
+alias ptemfxs='pytest -m "slow" -x -s' # Marked as slow and stop on first failure and show output
 
 alias ptenf='pytest --new-first' # Run the tests that have been added most recently first
 
