052ac529f7cf7d6904e9c2e6670bb08824a6b957
clementjumel <clement.jumel@gmail.com>
2024-01-12 22:54:18 +0100

    ⚡️ (oh-my-zsh): use pt prefix for pytest aliases

--- .config/oh-my-zsh/pytest.zsh
+++ .config/oh-my-zsh/pytest.zsh
@@ -1,20 +1,20 @@
-alias pe='pytest'
+alias pt='pytest'
 
-alias peff='pytest --failed-first' # Run the tests that failed last time first
+alias ptff='pytest --failed-first' # Run the tests that failed last time first
 
-alias pelf='pytest --last-failed' # Run only the tests that failed last time
+alias ptlf='pytest --last-failed' # Run only the tests that failed last time
 
-alias pem='pytest -m' # Run tests with a marker
-alias pemf='pytest -m "not slow"' # Marked as fast
-alias pemfx='pytest -m "not slow" -x' # Marked as fast and stop on first failure
-alias pemfxs='pytest -m "not slow" -x -s' # Marked as fast and stop on first failure and show output
-alias pems='pytest -m "slow"' # Marked as slow
-alias pemsx='pytest -m "slow" -x' # Marked as slow and stop on first failure
-alias pemfxs='pytest -m "slow" -x -s' # Marked as slow and stop on first failure and show output
+alias ptm='pytest -m' # Run tests with a marker
+alias ptmf='pytest -m "not slow"' # Marked as fast
+alias ptmfx='pytest -m "not slow" -x' # Marked as fast and stop on first failure
+alias ptmfxs='pytest -m "not slow" -x -s' # Marked as fast and stop on first failure and show output
+alias ptms='pytest -m "slow"' # Marked as slow
+alias ptmsx='pytest -m "slow" -x' # Marked as slow and stop on first failure
+alias ptmfxs='pytest -m "slow" -x -s' # Marked as slow and stop on first failure and show output
 
-alias penf='pytest --new-first' # Run the tests that have been added most recently first
+alias ptnf='pytest --new-first' # Run the tests that have been added most recently first
 
-alias pes='pytest -s' # Show output
+alias pts='pytest -s' # Show output
 
-alias pex='pytest --exitfirst' # Stop on first failure
-alias pexs='pytest --exitfirst -s' # Stop on first failure & show output
+alias ptx='pytest --exitfirst' # Stop on first failure
+alias ptxs='pytest --exitfirst -s' # Stop on first failure & show output
