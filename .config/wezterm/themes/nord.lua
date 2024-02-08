2f835110a4976727614bab1f7ea08821fa23554d
clementjumel <clement.jumel@gmail.com>
2024-02-09 00:23:54 +0100

    ✨ (wezterm): new rose-pine & nord themes

---
+++ .config/wezterm/themes/nord.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.color_scheme = "nord"
+M.window_background_opacity = 0.8
+
+return M
---
+++ .config/wezterm/themes/rose-pine-moon.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.color_scheme = "rose-pine-moon"
+M.window_background_opacity = 0.8
+
+return M
