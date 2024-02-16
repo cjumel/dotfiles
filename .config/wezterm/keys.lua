060f0f79160c4a5de191d4260be5c6de12dfe0ee
clementjumel <clement.jumel@gmail.com>
2024-02-16 18:46:09 +0100

    🐛 (wezterm): fix control-special-char combinations in wezterm

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -5,13 +5,42 @@
 local wezterm = require("wezterm")
 
 return {
+  -- [[ Fixes ]]
+
+  -- Fix control-key combinations with special characters
+  -- To be able to type them, these have been remapped with Karabiner, here we make the bridge
+  -- between the codes sent by Karabiner and the wanted terminal codes
+  -- Solution is inspired from here: https://github.com/wez/wezterm/issues/3180
+  -- See here for the supported terminal codes:
+  -- https://www.reddit.com/r/neovim/comments/okbag3/how_can_i_remap_ctrl_backspace_to_delete_a_word/
+  { -- From <C-%> on my keyboard to <C-_>
+    key = "_",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action({ SendString = "\x1f" }),
+  },
+  { -- From <C-`> on my keyboard to <C-^>
+    key = "6",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action({ SendString = "\x1e" }),
+  },
+  { -- From <C-[> on my keyboard to <C-]>
+    key = "$",
+    mods = "CTRL",
+    action = wezterm.action({ SendString = "\x1d" }),
+  },
+
+  -- [[ Control-command keymaps ]]
+
   {
-    -- Move-to-screen keymap causes WezTerm's window to change size, this keymap is convenient to
-    -- reset it to the default size
+    -- Move-to-screen keymap (control-command m) causes WezTerm's window to change size, this
+    -- keymap is convenient to reset it to the default size
     key = "r",
     mods = "CMD|CTRL",
     action = wezterm.action.ResetFontAndWindowSize,
   },
+
+  -- [[ Leader keymaps ]]
+
   {
     key = "o",
     mods = "LEADER",
