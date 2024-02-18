02b51c4d2748540553b284cf553cce9fa6559a73
clementjumel <clement.jumel@gmail.com>
2024-02-18 14:55:23 +0100

    🩹 (zsh): fix stow aliases and simplify them

--- .config/zsh/oh-my-zsh/stow.zsh
+++ .config/zsh/oh-my-zsh/stow.zsh
@@ -1,11 +1,5 @@
-alias st='stow'
-alias std='stow .' # Create symlinks for all files in the current directory
+alias st='stow --no-folding' # I only use stow with --no-folding, so let's make it the default
+alias std='stow --no-folding .' # Create symlinks for all files in the current directory
 
-alias sta='stow --adopt' # If a file already exists at the symlink target, adopt it with its changes
-alias stad='stow --adopt .'
-
-alias stn='stow --no-folding' # Force stow to create symlinks for all individual files
-alias stnd='stow --no-folding .'
-
-alias stan='stop --adopt --no-folding'
-alias stand='stop --adopt --no-folding .'
+alias sta='stow --no-folding --adopt' # If a file already exists at the symlink target, adopt it with its changes
+alias stad='stow --no-folding --adopt .'
