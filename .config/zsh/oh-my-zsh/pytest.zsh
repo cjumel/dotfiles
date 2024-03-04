4bc7bfc53e5056666f6c4b78df048ae287862bb7
clementjumel <clement.jumel@gmail.com>
2024-03-04 16:45:14 +0100

    🔥 (zsh): remove marker-specific aliases

--- .config/zsh/oh-my-zsh/pytest.zsh
+++ .config/zsh/oh-my-zsh/pytest.zsh
@@ -1,16 +1,12 @@
+# All aliases can be used with "-m <marker>" to run tests with a specific marker, however
+# I don't define aliases for it since it can be used in combination with any other alias
+# and the markers are project specific.
+
 alias pt='pytest'
 
 alias ptff='pytest --failed-first' # Run the tests that failed last time first
 
 alias ptlf='pytest --last-failed' # Run only the tests that failed last time
-
-alias ptm='pytest -m' # Run tests with a marker
-alias ptmf='pytest -m "not slow"' # Marked as fast
-alias ptmfx='pytest -m "not slow" -x' # Marked as fast and stop on first failure
-alias ptmfxs='pytest -m "not slow" -x -s' # Marked as fast and stop on first failure and show output
-alias ptms='pytest -m "slow"' # Marked as slow
-alias ptmsx='pytest -m "slow" -x' # Marked as slow and stop on first failure
-alias ptmfxs='pytest -m "slow" -x -s' # Marked as slow and stop on first failure and show output
 
 alias ptnf='pytest --new-first' # Run the tests that have been added most recently first
 
