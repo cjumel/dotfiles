885e210a87b80198fa1cf565d7367088710d812a
clementjumel <clement.jumel@gmail.com>
2024-03-27 11:58:33 +0100

    ✨ (oh-my-zsh): add Jupyter aliases

---
+++ .config/oh-my-zsh/custom/jupyter.zsh
@@ -0,0 +1,8 @@
+# Traditional Jupyter commands when installed with `pip` or similar package managers
+alias jp='jupyter'           # Main command
+alias jpl='jupyter lab'      # Sub-command to start the Jupyter Lab server
+alias jpn='jupyter notebook' # Sub-command to start the Jupyter Notebook server
+
+# Alternate Jupyter commands when Jupyter tools are installed with `pipx`
+alias jl='jupyter-lab'      # Sub-command to start the Jupyter Lab server
+alias jn='jupyter-notebook' # Sub-command to start the Jupyter Notebook server
