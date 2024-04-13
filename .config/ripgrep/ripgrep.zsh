8971f61aa036d757a5ff6964e70a227afc1bcf87
clementjumel <clement.jumel@gmail.com>
2024-04-13 13:16:22 +0200

    ✨ (ripgrep): alias to search in alias definitions

--- .config/ripgrep/ripgrep.zsh
+++ .config/ripgrep/ripgrep.zsh
@@ -5,14 +5,19 @@ export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep/ripgreprc
 
 # [[ Alias features ]]
 
-# Show the alias definition for the single alias corresponding to the provided string
+# Display the definition of the alias corresponding to the provided string
 function rg_alias() {
     rg --no-filename --no-line-number --type zsh "^alias $1=" ~/.config/
 }
-# Show the alias definitions for all aliases starting with the provided string
+# List the definitions of all the aliases starting with the provided string
 function rg_alias_list() {
     rg --no-filename --no-line-number --type zsh "^alias $1" ~/.config/
 }
+# Search in the definitions of all the aliases with to the provided string
+function rg_alias_search() {
+    rg --no-filename --no-line-number --type zsh "^alias .*$1" ~/.config/
+}
 
-alias al='rg_alias'       # (ripgrep) Show the alias definition for the single alias corresponding to the provided string
-alias all='rg_alias_list' # (ripgrep) Show the alias definitions for all aliases starting with the provided string
+alias al='rg_alias'         # (ripgrep) Display the definition of the alias corresponding to the provided string
+alias all='rg_alias_list'   # (ripgrep) List the definitions of all the aliases starting with the provided string
+alias als='rg_alias_search' # (ripgrep) Search in the definitions of all the aliases with to the provided string
