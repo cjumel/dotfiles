fee8213b9c3995cdfef8b722cdf61f7b9d4cbef1
clementjumel <clement.jumel@gmail.com>
2024-04-13 13:10:48 +0200

    🩹 (ripgrep): small fixes in ripgrep zsh script

--- .config/ripgrep/ripgrep.zsh
+++ .config/ripgrep/ripgrep.zsh
@@ -5,12 +5,12 @@ export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep/ripgreprc
 
 # [[ Alias features ]]
 
-# Find the first line of the alias definition for the single alias corresponding to the provided string
+# Show the alias definition for the single alias corresponding to the provided string
 function rg_alias() {
     rg --no-filename --no-line-number --type zsh "^alias $1=" ~/.config/
 }
-# Find the first line of the alias definitions for all aliases starting with the provided string
-rg_alias_list() {
+# Show the alias definitions for all aliases starting with the provided string
+function rg_alias_list() {
     rg --no-filename --no-line-number --type zsh "^alias $1" ~/.config/
 }
 
