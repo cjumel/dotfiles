a4d5cccb5bdd87a73e5f6305562591d80f290cb8
clementjumel <clement.jumel@gmail.com>
2024-04-13 14:51:17 +0200

    💡 (oh-my-zsh): improve comment in builtin alias aliases

--- .config/oh-my-zsh/custom/alias.zsh
+++ .config/oh-my-zsh/custom/alias.zsh
@@ -4,5 +4,5 @@ function alias_grep_list() {
 }
 
 # Builtin alias command aliases; useful as they list all aliases, even those defined outside of the `~/.config` directory
-alias alb='alias'            # List all aliases if no argument, or show an alias definition, or define a new alias (builtin alias command)
-alias albl='alias_grep_list' # List all aliases starting with the prefix passed as argument using the builtin alias command
+alias alb='alias'            # (builtin alias) List all aliases if no argument, or show an alias definition, or define a new alias
+alias albl='alias_grep_list' # (builtin alias) List all aliases starting with the prefix passed as argument using grep
