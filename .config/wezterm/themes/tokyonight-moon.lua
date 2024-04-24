e2cd4de5d9ac8b54a52ff73c661682406b57e934
clementjumel <clement.jumel@gmail.com>
2024-04-24 10:39:46 +0200

    🩹 (tmux): further improve tmux-tokyonight-moon theme

--- .config/wezterm/themes/tokyonight-moon.lua
+++ .config/wezterm/themes/tokyonight-moon.lua
@@ -1,7 +1,7 @@
 local config = {}
 
 config.color_scheme = "Tokyo Night Moon"
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.harfbuzz_features = {} -- Use ligatures
 
 return config
