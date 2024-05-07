4e21286793e81d5a8342d2ed42c00a20cf06c39c
clementjumel <clement.jumel@gmail.com>
2024-05-07 18:09:52 +0200

    ✨ (oh-my-zsh): alias to open Neovim without user configuration

--- .config/oh-my-zsh/custom/nvim.zsh
+++ .config/oh-my-zsh/custom/nvim.zsh
@@ -7,7 +7,9 @@ function nvim_contextual() {
     fi
 }
 
-alias vi='nvim_contextual'              # Open Neovim with contextual setup
-alias vic='rm -rf ~/.local/share/nvim/' # Clear Neovim files
-alias vil='nvim_contextual -c":e#<1"'   # Open Neovim on the last edited file
-alias vis='nvim'                        # Open Neovim and skip contextual setup
+alias vi='nvim_contextual'                           # Open Neovim with contextual setup
+alias vic='nvim --clean'                             # Open a clean Neovim instance, without any user configuration
+alias vil='nvim_contextual -c":e#<1"'                # Open Neovim on the last edited file
+alias vis='nvim'                                     # Open Neovim and skip contextual setup
+alias vix='echo "Would remove ~/.local/share/nvim/"' # Dry-run for deleting Neovim local files (not configuration files)
+alias vixf='rm -rf ~/.local/share/nvim/'             # Delete Neovim local files (not configuration files); useful to re-install Neovim from scratch
