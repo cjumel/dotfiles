f9e404bc7d48e9ba2beef5fc0f070837caf86558
clementjumel <clement.jumel@gmail.com>
2024-05-13 18:04:34 +0200

    🐛 (wezterm): fix broken theme change

--- .config/wezterm/theme/init.lua
+++ .config/wezterm/theme/init.lua
@@ -2,14 +2,18 @@ local utils = require("utils")
 
 local M = {}
 
-local ok, theme = pcall(require, "theme.current")
-if not ok then -- The theme symlink is missing
-  theme = require("theme.catppuccin-mocha") -- Default theme
-end
-
 --- Update the provided options with the theme ones.
 ---@param options table The options to update.
 ---@return table
-function M.make_options(options) return utils.concat_dicts({ options, theme }) end
+function M.make_options(options)
+  -- Force reload the theme module, otherwise it is not updated when the theme symlink is changed
+  package.loaded["theme.current"] = nil
+
+  local ok, theme = pcall(require, "theme.current")
+  if not ok then -- The theme symlink is missing
+    theme = require("theme.catppuccin-mocha") -- Default theme
+  end
+  return utils.concat_dicts({ options, theme })
+end
 
 return M
