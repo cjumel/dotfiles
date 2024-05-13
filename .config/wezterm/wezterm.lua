acbf5ee23608660c9fe5b506a12123aeb1c59346
clementjumel <clement.jumel@gmail.com>
2024-05-13 16:56:55 +0200

    🎨 (wezterm): restructure keys module

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,23 +1,13 @@
 local utils = require("utils")
 
--- [[ Define options ]]
+-- [[ Define base options ]]
 local options = require("options")
+
+-- [[ Apply theme-related options ]]
 local theme = require("theme")
 options = theme.make_options(options)
 
--- [[ Setup actions ]]
-local actions = require("actions")
-actions.set_actions()
-
--- [[ Define keymaps ]]
-local custom_keys = require("custom_keys")
-local default_keys = require("default_keys")
-local keys = {
-  keys = utils.concat_arrays({
-    default_keys.keys,
-    custom_keys.keys,
-  }),
-  key_tables = default_keys.key_tables,
-}
+-- [[ Define keys ]]
+local keys = require("keys")
 
 return utils.concat_dicts({ options, keys })
