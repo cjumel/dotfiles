cad04ced51597a4ce89ff59f6bcdc2ee4cc3a298
clementjumel <clement.jumel@gmail.com>
2024-05-14 11:36:24 +0200

    🐛 (werzterm): fix transparency keymaps behavior

--- .config/wezterm/keys/actions.lua
+++ .config/wezterm/keys/actions.lua
@@ -11,8 +11,9 @@ local M = {}
 function M.set_actions()
   wezterm.on("increase-transparency", function(window, _)
     local overrides = window:get_config_overrides() or {}
+    local options_ = theme.make_options(options)
     if not overrides.window_background_opacity then
-      overrides.window_background_opacity = 0.8
+      overrides.window_background_opacity = options_.window_background_opacity
     end
     local new_opacity = overrides.window_background_opacity - 0.05
     if new_opacity < 0.0 then
@@ -24,8 +25,9 @@ function M.set_actions()
 
   wezterm.on("decrease-transparency", function(window, _)
     local overrides = window:get_config_overrides() or {}
+    local options_ = theme.make_options(options)
     if not overrides.window_background_opacity then
-      overrides.window_background_opacity = 0.8
+      overrides.window_background_opacity = options_.window_background_opacity
     end
     local new_opacity = overrides.window_background_opacity + 0.05
     if new_opacity > 1.0 then
@@ -37,8 +39,8 @@ function M.set_actions()
 
   wezterm.on("reset-options", function(window, _)
     local overrides = window:get_config_overrides() or {}
-    options = theme.make_options(options)
-    overrides = utils.concat_dicts({ overrides, options })
+    local options_ = theme.make_options(options, { force_reload = true })
+    overrides = utils.concat_dicts({ overrides, options_ })
     window:set_config_overrides(overrides)
   end)
 end
--- .config/wezterm/theme/init.lua
+++ .config/wezterm/theme/init.lua
@@ -4,10 +4,16 @@ local M = {}
 
 --- Update the provided options with the theme ones.
 ---@param options table The options to update.
+---@param opts table|nil Options to customize the function behavior.
 ---@return table
-function M.make_options(options)
+function M.make_options(options, opts)
+  opts = opts or {}
+  local force_reload = opts.force_reload or false
+
   -- Force reload the theme module, otherwise it is not updated when the theme symlink is changed
-  package.loaded["theme.current"] = nil
+  if force_reload then
+    package.loaded["theme.current"] = nil
+  end
 
   local ok, theme = pcall(require, "theme.current")
   if not ok then -- The theme symlink is missing
