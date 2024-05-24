5c31c7ae059723bb0c5c9e91dae54ffa48466feb
clementjumel <clement.jumel@gmail.com>
2024-05-24 18:21:58 +0200

    ✨ (zoxide): `ci` alias for `cdi`

--- .config/zoxide/zoxide.zsh
+++ .config/zoxide/zoxide.zsh
@@ -1,3 +1,7 @@
 # Initialize zoxide
 # `--cmd cd` will make zoxide automatically use `cd` & `cdi`
 eval "$(zoxide init --cmd cd zsh)"
+
+# `c` is globally aliased to `cd`
+# Let's alias `ci` to `cdi` but only when using zoxide
+alias ci='cdi'
