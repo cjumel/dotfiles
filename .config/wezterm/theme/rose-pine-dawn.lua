bb34372e4db4ce8fa6fd44213b3ff165f20f81a3
clementjumel <clement.jumel@gmail.com>
2024-05-24 10:51:48 +0200

    🩹 (wezterm): make more opaque some light themes

--- .config/wezterm/theme/rose-pine-dawn.lua
+++ .config/wezterm/theme/rose-pine-dawn.lua
@@ -1,5 +1,6 @@
 local config = {}
 
 config.color_scheme = "rose-pine-dawn"
+config.window_background_opacity = 0.95
 
 return config
--- .config/wezterm/theme/tokyonight-day.lua
+++ .config/wezterm/theme/tokyonight-day.lua
@@ -1,5 +1,6 @@
 local config = {}
 
 config.color_scheme = "Tokyo Night Day"
+config.window_background_opacity = 0.95
 
 return config
