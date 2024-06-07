bee32974d2a0987dd9661f99b17087d6bb44b113
clementjumel <clement.jumel@gmail.com>
2024-06-07 14:14:01 +0200

    🔥 (wezterm): remove Wetzerm full screen keymap
    I've started to use Raycast, which supports the same feature,
    however Raycast's feature doesn't suffer from an issue of Wezterm's
    where Alt-Tab couldn't find the Wezterm window when in Wezterm's
    full screen mode.

--- .config/wezterm/keys/custom_keys.lua
+++ .config/wezterm/keys/custom_keys.lua
@@ -1,7 +1,3 @@
--- In addition to the keymaps setup here, I introduced in MacOS system settings custom application
--- keyboard mappings, cmd-ctrl-f for "Enter Full Screen" and "Exit Full Screen", in order
--- to use MacOS native full screen mode, in addtion to WezTerm full screen mode.
-
 local wezterm = require("wezterm")
 
 local act = wezterm.action
@@ -9,14 +5,8 @@ local act = wezterm.action
 return {
   keys = {
     -- [[ Control-command keymaps ]]
-    -- Keymaps similar to MacOS builtin (e.g. <C-M-f> to toggle full screen) and which should be
-    -- convenient to do with similar key strokes
+    -- Keymaps to manipulate the terminal application
 
-    {
-      key = "e", -- Enter Wezterm's full screen mode
-      mods = "CMD|CTRL",
-      action = act.ToggleFullScreen,
-    },
     {
       key = "r",
       mods = "CMD|CTRL",
@@ -44,16 +34,15 @@ return {
     },
 
     -- [[ Control keymaps ]]
-    -- Enable some control keymaps with special characters by sending the right string given an
-    -- input sequence
-    -- This is inspired from: https://github.com/wez/wezterm/issues/3180
-    -- See here for terminal codes supported by Neovim:
+    -- Enable some control keymaps with special characters by sending the right string given an input sequence
+    -- This is inspired by https://github.com/wez/wezterm/issues/3180, & for terminal codes supported by Neovim see
     -- https://www.reddit.com/r/neovim/comments/okbag3/how_can_i_remap_ctrl_backspace_to_delete_a_word/
-
+    --
     -- Some control keymaps with special characters are already working:
-    -- - <C-^> on my keyboard corresponds to <C-[> (escape)
-    -- - <C-$> on my keyboard corresponds to <C-]>
-    -- - <C-`> on my keyboard corresponds to <C-\>
+    --  - <C-^> on my keyboard corresponds to <C-[> (escape)
+    --  - <C-$> on my keyboard corresponds to <C-]>
+    --  - <C-`> on my keyboard corresponds to <C-\>
+
     {
       key = "phys:Quote", -- % key on my keyboard
       mods = "CTRL",
