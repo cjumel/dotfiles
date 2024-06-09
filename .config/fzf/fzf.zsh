2de19fb298c0f8e426c9b8f3f9da47450daab81c
clementjumel <clement.jumel@gmail.com>
2024-06-09 22:17:28 +0200

    🩹 (fzf): some small improvements in all the config

--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -1,33 +1,22 @@
 # [[ General options ]]
 
-# Prompts
-export FZF_FILE_PROMPT='File > '
-export FZF_DIR_PROMPT='Directory > '
-export FZF_CMD_PROMPT='Command > '
-export FZF_ALIAS_PROMPT='Alias > '
-export FZF_THEME_PROMPT='Theme > '
-
-# `fd` commands
-export FZF_FD_COMMAND='fd .'
-export FZF_FD_COMMAND_HIDDEN='fd --hidden .'
+# Source commands
+export FZF_FD_COMMAND='fd --hidden .'
 export FZF_FD_COMMAND_ALL='fd --hidden --no-ignore --exclude .git .'
-export FZF_FD_FILE_COMMAND='fd --type f .'
-export FZF_FD_FILE_COMMAND_HIDDEN='fd --type f --hidden .'
+export FZF_FD_FILE_COMMAND='fd --type f --hidden .'
 export FZF_FD_FILE_COMMAND_ALL='fd --type f --hidden --no-ignore --exclude .git .'
-export FZF_FD_DIR_COMMAND='fd --type d .'
-export FZF_FD_DIR_COMMAND_HIDDEN='fd --type d --hidden .'
+export FZF_FD_DIR_COMMAND='fd --type d --hidden .'
 export FZF_FD_DIR_COMMAND_ALL='fd --type d --hidden --no-ignore --exclude .git .'
 
-# Previewers
-# Escaped versions are necessary to change the preview in transform actions
+# Previewers (escaped versions are necessary for transform actions)
 export FZF_FILE_PREVIEW='bat --color=always --line-range=:500 {}'
 export FZF_FILE_PREVIEW_ESCAPED='bat --color=always --line-range=:500 \{}'
 export FZF_DIR_PREVIEW='eza -a1 --color=always {}'
 export FZF_DIR_PREVIEW_ESCAPED='eza -a1 --color=always \{}'
 
-# General `fzf` options
+# General options
 export FZF_DEFAULT_COMMAND="$FZF_FD_COMMAND"
-# ctrl-g is chosen for "go to top", like (Neo)vim's `gg`
+# <Ctrl-g> is chosen for "go to top", like (Neo)vim's `gg`
 export FZF_DEFAULT_OPTS="
     --layout=reverse
     --height=50%
@@ -41,42 +30,27 @@ export FZF_DEFAULT_OPTS="
 # - <Ctrl-T> to insert a file or directory path in the command line
 # - <Ctrl-R> to insert a command from the command history
 # - <Alt-C> to `cd` to a directory
+
 # Enable <Alt-C> keymap (alt mappings are not available on my system)
 bindkey "©" fzf-cd-widget
 
 # <Ctrl-T>
-# By default, start with directory search as looking for a file naturally starts by looking for its parent directory anyway
 export FZF_CTRL_T_COMMAND="$FZF_FD_FILE_COMMAND"
 export FZF_CTRL_T_TOGGLE_TRANSFORMER="
     if [[ {fzf:prompt} = \"File > \" ]]; then
         echo \"change-prompt(Directory > )+reload($FZF_FD_DIR_COMMAND)+change-preview($FZF_DIR_PREVIEW_ESCAPED)\"
-    elif [[ {fzf:prompt} = \"File (w/ hidden) > \" ]]; then
-        echo \"change-prompt(Directory (w/ hidden) > )+reload($FZF_FD_DIR_COMMAND_HIDDEN)+change-preview($FZF_DIR_PREVIEW_ESCAPED)\"
     elif [[ {fzf:prompt} = \"File (all) > \" ]]; then
         echo \"change-prompt(Directory (all) > )+reload($FZF_FD_DIR_COMMAND_ALL)+change-preview($FZF_DIR_PREVIEW_ESCAPED)\"
     elif [[ {fzf:prompt} = \"Directory > \" ]]; then
         echo \"change-prompt(File > )+reload($FZF_FD_FILE_COMMAND)+change-preview($FZF_FILE_PREVIEW_ESCAPED)\"
-    elif [[ {fzf:prompt} = \"Directory (w/ hidden) > \" ]]; then
-        echo \"change-prompt(File (w/ hidden) > )+reload($FZF_FD_FILE_COMMAND_HIDDEN)+change-preview($FZF_FILE_PREVIEW_ESCAPED)\"
     elif [[ {fzf:prompt} = \"Directory (all) > \" ]]; then
         echo \"change-prompt(File (all) > )+reload($FZF_FD_FILE_COMMAND_ALL)+change-preview($FZF_FILE_PREVIEW_ESCAPED)\"
     fi
 "
-export FZF_CTRL_T_HIDDEN_TRANSFORMER="
-    if [[ {fzf:prompt} = \"File > \" ]] || [[ {fzf:prompt} = \"File (all) > \" ]]; then
-        echo \"reload(eval $FZF_FD_FILE_COMMAND_HIDDEN)+change-prompt(File (w/ hidden) > )\"
-    elif [[ {fzf:prompt} = \"Directory > \" ]] || [[ {fzf:prompt} = \"Directory (all) > \" ]]; then
-        echo \"reload(eval $FZF_FD_DIR_COMMAND_HIDDEN)+change-prompt(Directory (w/ hidden) > )\"
-    elif [[ {fzf:prompt} = \"File (w/ hidden) > \" ]]; then
-        echo \"reload(eval $FZF_FD_FILE_COMMAND)+change-prompt(File > )\"
-    elif [[ {fzf:prompt} = \"Directory (w/ hidden) > \" ]]; then
-        echo \"reload(eval $FZF_FD_DIR_COMMAND)+change-prompt(Directory > )\"
-    fi
-"
 export FZF_CTRL_T_ALL_TRANSFORMER="
-    if [[ {fzf:prompt} = \"File > \" ]] || [[ {fzf:prompt} = \"File (w/ hidden) > \" ]]; then
+    if [[ {fzf:prompt} = \"File > \" ]]; then
         echo \"reload(eval $FZF_FD_FILE_COMMAND_ALL)+change-prompt(File (all) > )\"
-    elif [[ {fzf:prompt} = \"Directory > \" ]] || [[ {fzf:prompt} = \"Directory (w/ hidden) > \" ]]; then
+    elif [[ {fzf:prompt} = \"Directory > \" ]]; then
         echo \"reload(eval $FZF_FD_DIR_COMMAND_ALL)+change-prompt(Directory (all) > )\"
     elif [[ {fzf:prompt} = \"File (all) > \" ]]; then
         echo \"reload(eval $FZF_FD_FILE_COMMAND)+change-prompt(File > )\"
@@ -85,41 +59,34 @@ export FZF_CTRL_T_ALL_TRANSFORMER="
     fi
 "
 
+# <Ctrl-]> corresponds to <Ctrl-$> on my keyboard
 export FZF_CTRL_T_OPTS="
-    --prompt '$FZF_FILE_PROMPT'
+    --prompt 'File > '
     --preview '$FZF_FILE_PREVIEW'
     --bind 'ctrl-t:transform: $FZF_CTRL_T_TOGGLE_TRANSFORMER'
-    --bind 'ctrl-^:transform: $FZF_CTRL_T_HIDDEN_TRANSFORMER'
-    --bind 'ctrl-_:transform: $FZF_CTRL_T_ALL_TRANSFORMER'
+    --bind 'ctrl-]:transform: $FZF_CTRL_T_ALL_TRANSFORMER'
 "
 
 # <Ctrl-R>
 export FZF_CTRL_R_OPTS="
-    --prompt '$FZF_CMD_PROMPT'
+    --prompt 'Command > '
 "
 
 # <Alt-C>
 export FZF_ALT_C_COMMAND="$FZF_FD_DIR_COMMAND"
-export FZF_ALT_C_HIDDEN_TRANSFORMER="
-    if [[ {fzf:prompt} = \"Directory > \" ]] || [[ {fzf:prompt} = \"Directory (all) > \" ]]; then
-        echo \"reload(eval $FZF_FD_DIR_COMMAND_HIDDEN)+change-prompt(Directory (w/ hidden) > )\"
-    elif [[ {fzf:prompt} = \"Directory (w/ hidden) > \" ]]; then
-        echo \"reload(eval $FZF_FD_DIR_COMMAND)+change-prompt(Directory > )\"
-    fi
-"
-export FZF_ALT_C_HIDDEN_AND_IGNORE_TRANSFORMER="
-    if [[ {fzf:prompt} = \"Directory > \" ]] || [[ {fzf:prompt} = \"Directory (w/ hidden) > \" ]]; then
+export FZF_ALT_C_ALL_TRANSFORMER="
+    if [[ {fzf:prompt} = \"Directory > \" ]]; then
         echo \"reload(eval $FZF_FD_DIR_COMMAND_ALL)+change-prompt(Directory (all) > )\"
     elif [[ {fzf:prompt} = \"Directory (all) > \" ]]; then
         echo \"reload(eval $FZF_FD_DIR_COMMAND)+change-prompt(Directory > )\"
     fi
 "
 
+# <Ctrl-]> corresponds to <Ctrl-$> on my keyboard
 export FZF_ALT_C_OPTS="
-    --prompt '$FZF_DIR_PROMPT'
+    --prompt 'Directory > '
     --preview '$FZF_DIR_PREVIEW'
-    --bind 'ctrl-^:transform: $FZF_ALT_C_HIDDEN_TRANSFORMER'
-    --bind 'ctrl-_:transform: $FZF_ALT_C_HIDDEN_AND_IGNORE_TRANSFORMER'
+    --bind 'ctrl-]:transform: $FZF_ALT_C_ALL_TRANSFORMER'
 "
 
 # [[ Completion ]]
@@ -131,14 +98,13 @@ export FZF_ALT_C_OPTS="
 export FZF_COMPLETION_TRIGGER=''
 bindkey '^O' fzf-completion
 
-# Use `fd` to generate completion candidates
-# In this use case, `fd` doesn't respect the `ignore` file
+# Use `fd` to generate completion candidates (in this use case, `fd` doesn't respect the `ignore` file)
 # In the following functions, `$1` is the base path to start traversal
 _fzf_compgen_path() {
-    fd --hidden --no-ignore --exclude ".git" --follow . "$1"
+    fd --hidden --follow . "$1"
 }
 _fzf_compgen_dir() {
-    fd --hidden --no-ignore --exclude ".git" --follow --type d . "$1"
+    fd --hidden --follow --type d . "$1"
 }
 
 # Advanced customization of fzf options via _fzf_comprun function
@@ -204,7 +170,7 @@ tokyonight-storm'
 # Pick a theme with fzf and set it up by creating the relevant symlinks
 function theme_fzf_picker() {
     # Make the user select a theme using fzf among the available ones
-    selected_theme=$(echo "$TERMINAL_THEMES" | fzf --prompt="$FZF_THEME_PROMPT")
+    selected_theme=$(echo "$TERMINAL_THEMES" | fzf --prompt="Theme > ")
 
     # Exit if no theme is selected, to avoid creating broken symlinks
     if [[ -z $selected_theme ]]; then
