ec879571d9c32acba4efeb8c9cdcf9ccc2e1a19b
clementjumel <clement.jumel@gmail.com>
2024-06-13 00:31:37 +0200

    💡 (oh-my-zsh): document & simplify tmux aliases

--- .config/oh-my-zsh/custom/tmux.zsh
+++ .config/oh-my-zsh/custom/tmux.zsh
@@ -1,23 +1,18 @@
-alias t='tmux new'
+alias tm='tmux' # Base Tmux command, create a new unnamed session when used alone
 
-alias tm='tmux'
+alias tma='tmux attach'     # Attach: attach to a target session or the last one when used alone
+alias tmat='tmux attach -t' # Attach target: attach to a session by name
 
-alias tma='tmux attach'     # Attach to last session
-alias tmat='tmux attach -t' # Attach to session with target name
+alias tmd='tmux detach' # Detach: detach from the current session but don't kill it
 
-alias tmd='tmux detach'
+alias tml='tmux ls' # List: list all existing sessions
 
-alias tml='tmux ls'
+alias tmn='tmux new'     # Nex: create a new session, unnamed when used alone
+alias tmns='tmux new -s' # New session: create a new named sessions
 
-alias tmn='tmux new'     # New unnamed session
-alias tmns='tmux new -s' # New named sessions
+alias tmk='tmux kill-session'     # Kill: kill an existing session
+alias tmkt='tmux kill-session -t' # Kill target: kill an existing session by name
 
-alias tmk='tmux kill-session'
-alias tmkt='tmux kill-session -t' # Kill session with target name
+alias tmK='tmux kill-server' # Kill server: kill the Tmux server
 
-alias tmK='tmux kill-server'
-
-alias tmr='tmux rename'
-
-alias tms='tmux source'
-alias tmsd='tmux source ~/.config/tmux/tmux.conf' # Source default configuration
+alias tmr='tmux rename' # Rename: rename the current session
