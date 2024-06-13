fb368256f20c9730c2fbff1059e520c699bcf754
clementjumel <clement.jumel@gmail.com>
2024-06-13 11:09:00 +0200

    🚚 (oh-my-zsh): move zoxide alias to oh-my-zsh

--- .config/zoxide/zoxide.zsh
+++ .config/zoxide/zoxide.zsh
@@ -1,7 +1,2 @@
-# Initialize zoxide
-# `--cmd cd` will make zoxide automatically use `cd` & `cdi`
-eval "$(zoxide init --cmd cd zsh)"
-
-# `c` is globally aliased to `cd`
-# Let's alias `ci` to `cdi` but only when using zoxide
-alias ci='cdi'
+# Initialize zoxide; it will define `z` & `zi` commands
+eval "$(zoxide init zsh)"
