459842e6db4823cbb686493ab5638b4f516fcd2c
clementjumel <clement.jumel@gmail.com>
2024-06-13 11:24:44 +0200

    🚚 (oh-my-zsh): move ipython aliases to oh-my-zsh

--- .config/ipython/ipython.zsh
+++ .config/ipython/ipython.zsh
@@ -1,9 +1,2 @@
-# [[ Global configuration ]]
-
 # Set the iPython config directory
 export IPYTHONDIR="$HOME/.config/ipython"
-
-# [[ Aliases ]]
-
-alias ipy='ipython'                  # Start ipython
-alias ipyn='ipython --no-autoindent' # Start ipython without autoindent (useful for pasting indented code)
---
+++ .config/oh-my-zsh/custom/ipython.zsh
@@ -0,0 +1,2 @@
+alias ipy='ipython'                  # ipython: start ipython
+alias ipyn='ipython --no-autoindent' # ipython no-autoindent: start ipython without autoindent (useful for pasting indented code)
