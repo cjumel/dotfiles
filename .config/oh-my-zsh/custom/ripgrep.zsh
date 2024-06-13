8603158ceb8e5a0947cdd86556febf91c5d7e671
clementjumel <clement.jumel@gmail.com>
2024-06-13 11:00:01 +0200

    ✨ (oh-my-zsh): improve alias definition features

--- .config/oh-my-zsh/custom/ripgrep.zsh
+++ .config/oh-my-zsh/custom/ripgrep.zsh
@@ -1,16 +1,16 @@
-# Display the definition of the alias corresponding to the provided string
-function rg_alias() {
-    rg --no-filename --no-line-number --type zsh "^alias $1=" ~/.config/
+# Display the definition of the alias corresponding to the provided string in the custom alias directory
+function rg_alias_definition() {
+    rg --no-filename --no-line-number --type zsh --max-depth 1 "^alias $1=" ~/.config/oh-my-zsh/custom/
 }
-# List the definitions of all the aliases starting with the provided string
-function rg_alias_list() {
-    rg --no-filename --no-line-number --type zsh "^alias $1" ~/.config/
+# Display the definitions of all the aliases corresponding to the provided string in the custom alias directory
+function rg_alias_definition_list() {
+    rg --no-filename --no-line-number --type zsh --max-depth 1 "^alias $1" ~/.config/oh-my-zsh/custom/
 }
-# Search in the definitions of all the aliases with to the provided string
-function rg_alias_search() {
-    rg --no-filename --no-line-number --type zsh "^alias .*$1" ~/.config/
+# Search in the definitions of all the aliases corresponding to the provided string in the custom alias directory
+function rg_alias_definition_search() {
+    rg --no-filename --no-line-number --type zsh --max-depth 1 "^alias .*$1" ~/.config/oh-my-zsh/custom/
 }
 
-alias ad='rg_alias'         # Alias definition: display the definition of the alias corresponding to the provided string
-alias adl='rg_alias_list'   # Alias definition list: list the definitions of all the aliases starting with the provided string
-alias ads='rg_alias_search' # Alias definition search: search the provided string in the definitions of all the aliases
+alias ad='rg_alias_definition'         # Alias definition: display the definition of the alias corresponding to the provided string in the custom alias directory
+alias adl='rg_alias_definition_list'   # Alias definition list: display the definitions of all the aliases corresponding to the provided string in the custom alias directory
+alias ads='rg_alias_definition_search' # Alias definition search: search in the definitions of all the aliases corresponding to the provided string in the custom alias directory
