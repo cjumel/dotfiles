0ab3279a2998e159c0ab6052ed23b4addd7c6d45
clementjumel <clement.jumel@gmail.com>
2024-06-17 15:17:37 +0200

    ⚡️ (zsh): migrate oh-my-zsh plugins to zinit

--- .config/oh-my-zsh/oh-my-zsh.zsh
+++ .config/oh-my-zsh/oh-my-zsh.zsh
@@ -19,24 +19,6 @@ ZSH_CUSTOM=$HOME/.config/oh-my-zsh/custom/
 
 # Specify plugins to load
 # Standard plugins can be found in $ZSH/plugins/, while custom ones may be added to $ZSH_CUSTOM/plugins/
-plugins=(
-    fzf-tab                 # Provide tab-completion with fzf, enabling for instance fuzzy finding & previewing (should be loaded before plugins like zsh-autosuggestions)
-    zsh-syntax-highlighting # Provide syntax highlighting of commands while typing, indicating for instance if command is valid
-    zsh-autosuggestions     # Provide virtual text suggestions based on the history of commands
-)
-
-# fzf-tab config
-zstyle ':completion:*' menu no            # Suggested by the documentation
-zstyle ':fzf-tab:*' fzf-flags --height=30 # Prevent fzf window from being too small when few completions are available
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -a1 --color=always $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -a1 --color=always $realpath'
-
-# zsh-completions config
-# zsh-completions provides additional completion for a variety of CLI tools; it can't be installed properly using oh-my-zsh regular plugin
-# system, instead the following line must be added before `source "$ZSH"/oh-my-zsh.sh`
-fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
-
-# zsh-autosuggestions config
-bindkey '^y' autosuggest-accept
+plugins=()
 
 source "$ZSH"/oh-my-zsh.sh
--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -15,3 +15,27 @@ source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 ### End of Zinit's installer chunk
+
+# [[ fzf-tab ]]
+# `fzf-tab` provides a tab-completion implementation with `fzf`, enabling fuzzy finding & previewing candidates
+# `fzf-tab` should be loaded before plugins like `zsh-autosuggestions`
+
+zinit light Aloxaf/fzf-tab
+
+# Force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no            
+# Prevent fzf window from being too small when a small number of completions is available
+zstyle ':fzf-tab:*' fzf-flags --height=30 
+# Preview directory's content with `eza` when completing the builtin `cd` or `zoxide`
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -a1 --color=always $realpath'
+
+# [[ Zsh standard plugins ]]
+# A few standard plugins for zsh, enabling syntax highlighting (show valid/invalid commands, paths, etc.), autosuggestions (suggest 
+# commands in ghost text based on command history) & additional tool completions (e.g. for `pre-commit`)
+
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
+
+bindkey '^y' autosuggest-accept
