958795b2d0f8eff3007630e3c3778d45f831d443
clementjumel <clement.jumel@gmail.com>
2024-06-17 14:38:44 +0200

    💡 (oh-my-zsh): improve code comments

--- .config/oh-my-zsh/oh-my-zsh.zsh
+++ .config/oh-my-zsh/oh-my-zsh.zsh
@@ -19,28 +19,24 @@ ZSH_CUSTOM=$HOME/.config/oh-my-zsh/custom/
 
 # Specify plugins to load
 # Standard plugins can be found in $ZSH/plugins/, while custom ones may be added to $ZSH_CUSTOM/plugins/
-# fzf-tab re-implementes Tab-completion using fzf, enabling fuzzy finding & previewing everything supported by Tab-completion
-#   fzf-tab needs to be loaded before plugins like zsh-autosuggestions
-# zsh-syntax-highlighting provides highlighting of commands while typing, indicating for instance valid and invalid ones
-# zsh-autosuggestions provides virtual text suggestions based on the history of commands
 plugins=(
-    fzf-tab
-    zsh-syntax-highlighting
-    zsh-autosuggestions
+    fzf-tab                 # Provide tab-completion with fzf, enabling for instance fuzzy finding & previewing (should be loaded before plugins like zsh-autosuggestions)
+    zsh-syntax-highlighting # Provide syntax highlighting of commands while typing, indicating for instance if command is valid
+    zsh-autosuggestions     # Provide virtual text suggestions based on the history of commands
 )
 
-# fzf-tab
+# fzf-tab config
 zstyle ':completion:*' menu no            # Suggested by the documentation
 zstyle ':fzf-tab:*' fzf-flags --height=30 # Prevent fzf window from being too small when few completions are available
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -a1 --color=always $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -a1 --color=always $realpath'
 
-# zsh-completions
+# zsh-completions config
 # zsh-completions provides additional completion for a variety of CLI tools; it can't be installed properly using oh-my-zsh regular plugin
 # system, instead the following line must be added before `source "$ZSH"/oh-my-zsh.sh`
 fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
 
-# zsh-autosuggestions
+# zsh-autosuggestions config
 bindkey '^y' autosuggest-accept
 
 source "$ZSH"/oh-my-zsh.sh
