320396d31a80ac38f743e74abaf4dec3f5a4fcd9
clementjumel <clement.jumel@gmail.com>
2024-06-18 14:43:29 +0200

    🔧 (zsh): improve fzf-tab settings

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,18 +1,16 @@
+# Disable warning on expression not expanding in single quotes (not relevant with zstyle calls for fzf-tab)
+# shellcheck disable=SC2016
+
 # [[ fzf-tab ]]
 # `fzf-tab` provides a tab-completion implementation with `fzf`, enabling fuzzy finding & previewing candidates
 # `fzf-tab` should be loaded before plugins like `zsh-autosuggestions`
 
 zinit light Aloxaf/fzf-tab
 
-# Force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
-zstyle ':completion:*' menu no
-# Prevent fzf window from being too small when a small number of completions is available
-zstyle ':fzf-tab:*' fzf-flags --height=30
-# Preview directory's content with `eza` when completing the builtin `cd` or `zoxide`
-# shellcheck disable=SC2016
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -a1 --color=always $realpath'
-# shellcheck disable=SC2016
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':completion:*' menu no                                                         # Disable builtin tab completion menu
+zstyle ':fzf-tab:*' fzf-flags --height=20                                              # Add height to fzf window (otherwise it can be too small)
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -a1 --color=always $realpath'         # Enable preview for cd command
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -a1 --color=always $realpath' # Enable preview for zoxide command
 
 # [[ Zsh standard plugins ]]
 # A few standard plugins for zsh, enabling syntax highlighting (show valid/invalid commands, paths, etc.), autosuggestions (suggest
