5ec308e11873601c950bc5848bd7433e8cf86638
clementjumel <clement.jumel@gmail.com>
2024-06-18 11:05:40 +0200

    🔥 (zsh): remove Stow aliases
    I only use Stow for dotfiles, & in that case I prefer to use
    the Stow command from the Makefile directly.

--- .config/zsh/aliases/stow.zsh
+++
@@ -1,2 +0,0 @@
-alias st='stow --no-folding'          # I only use stow with --no-folding, so let's make it the default
-alias sta='stow --no-folding --adopt' # Like `stow --no-folding` but if a file already exists at the symlink target, adopt it with its changes
