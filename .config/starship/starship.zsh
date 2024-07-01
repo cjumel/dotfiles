1135399fd6952d81f0cfc41c1fa913bec5f9f6bf
clementjumel <clement.jumel@gmail.com>
2024-07-01 11:50:28 +0200

    🐛 (starship): fix undesired new line at top of terminal

--- .config/starship/starship.zsh
+++ .config/starship/starship.zsh
@@ -1,5 +1,11 @@
 # Change starship configuration file location
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
 
+# Fix undesired new line at top top of terminal when entering or clearing it, when `add_new_line` is `true`
+# Solution is taken from https://github.com/starship/starship/issues/560, and should be used with `add_new_line` set to `false`
+# shellcheck disable=SC1009,SC1056,SC1072,SC1073,SC1083
+precmd() { precmd() { echo "" } }
+alias clear="precmd() { precmd() { echo } } && clear"
+
 # Initialize starship
 eval "$(starship init zsh)"
