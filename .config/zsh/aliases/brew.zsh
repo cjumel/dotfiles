636e31dc6a0982bbee912891604c904960fd0519
clementjumel <clement.jumel@gmail.com>
2024-07-25 11:21:09 +0200

    ✨ (zsh): complete & document brew aliases

--- .config/zsh/aliases/brew.zsh
+++ .config/zsh/aliases/brew.zsh
@@ -1,27 +1,29 @@
-alias bb='brew bundle'
-alias bbd='brew bundle dump' # Write the installed brew formulae & casks in a Brewfile
+alias bb='brew bundle'       # [B]undle: base command for Brewfile management
+alias bbd='brew bundle dump' # [B]undle [D]ump: write the installed formulae & casks in a Brewfile
 
-alias bc='brew cleanup'
+alias bc='brew cleanup' # [C]leanup: remove old versions of installed formulae & casks
 
-alias bi='brew install'
-alias bic='brew install --cask'
+alias bi='brew install'         # [I]nstall: install a formula or cask
+alias bic='brew install --cask' # [I]nstall [C]ask: install a cask
 
-alias bif='brew info'
+alias bif='brew info' # [I]n[F]o: show information about a formula or cask
 
-alias bl='brew list'
-alias blc='brew list --cask'
+alias bl='brew list'            # [L]ist: list installed formulae & casks
+alias blc='brew list --cask'    # [L]ist [C]ask: list installed casks
+alias blf='brew list --formula' # [L]ist [F]ormula: list installed formulae
 
-alias blv='brew leaves' # List installed formulae that are not dependencies of another one
+alias blv='brew leaves' # [L]ea[V]es: list installed formulae which are not the dependency of another one
 
-alias bo='brew outdated'
+alias bo='brew outdated'           # [O]utdated: list outdated formulae & casks
+alias bog='brew outdated --greedy' # [O]utdated [G]reedy: list outdated formulae & casks, including casks with auto-updates or version "latest"
 
-alias bp='brew pin'
+alias bp='brew pin' # [P]in: prevent a formula or cask from being upgraded
 
-alias bud='brew update' # Update homebrew itself
+alias bud='brew update' # [U]p[D]ate: update Homebrew itself
 
-alias bug='brew upgrade'           # Update all outdated formulae and casks
-alias bugg='brew upgrade --greedy' # Include casks with auto-updates or version "latest"
+alias bug='brew upgrade'           # [U]p[G]rade: update a target formula or cask or all of them
+alias bugg='brew upgrade --greedy' # [U]p[G]rade [G]reedy: update a target formula or cask or all of them, including casks with auto-updates or version "latest"
 
-alias bui='brew uninstall'
+alias bui='brew uninstall' # [U]n[I]nstall: uninstall a formula or cask
 
-alias bup='brew unpin'
+alias bup='brew unpin' # [U]n[P]in: allow a formula or cask to be upgraded
