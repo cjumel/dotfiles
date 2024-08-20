7e6fbf23e30eee232aa6a67327c91ea521cf9854
clementjumel <clement.jumel@gmail.com>
2024-08-20 11:14:24 +0200

    🩹 (wezterm): make gruvbox themes background opaque

--- .config/wezterm/theme/gruvbox-dark.lua
+++ .config/wezterm/theme/gruvbox-dark.lua
@@ -1,5 +1,6 @@
 local config = {}
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
+config.window_background_opacity = 1.00
 
 return config
--- .config/wezterm/theme/gruvbox-light.lua
+++ .config/wezterm/theme/gruvbox-light.lua
@@ -1,6 +1,6 @@
 local config = {}
 
 config.color_scheme = "Gruvbox (Gogh)"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 1.00
 
 return config
