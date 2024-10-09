28a5b5ac53f929bebf2c3d2854768dbf29150211
clementjumel <clement.jumel@gmail.com>
2024-10-09 15:05:57 +0200

    ✨ (zsh): small improvements of fzf-tab plugin config

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -7,10 +7,26 @@
 
 zinit light Aloxaf/fzf-tab
 
-zstyle ':completion:*' menu no                                                         # Disable builtin tab completion menu
-zstyle ':fzf-tab:*' fzf-flags '--height=40%'                                           # Specify height to fzf window (prevent from being too small)
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -a1 --color=always $realpath'         # Enable preview for cd command
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -a1 --color=always $realpath' # Enable preview for zoxide command
+zstyle ':completion:*' menu no # Disable builtin tab completion menu
+
+# Specify the `fzf` options to use (note that the ones specify in `FZF_DEFAULT_OPTS` will not be taken into account):
+#   - add a specified height, to prevent the `fzf` window from being too small when a few items are suggested
+#   - add some keybindings also defined in `fzf`
+zstyle ':fzf-tab:*' fzf-flags \
+    --height=40% \
+    --bind 'ctrl-]:toggle-preview' \
+    --bind 'ctrl-g:top' \
+    --bind 'ctrl-^:forward-word' \
+    --bind 'ctrl-_:backward-word'
+
+# Enable preview for a few specific commands
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':fzf-tab:complete:ls:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':fzf-tab:complete:eza:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':fzf-tab:complete:cp:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':fzf-tab:complete:mv:*' fzf-preview 'eza -a1 --color=always $realpath'
+zstyle ':fzf-tab:complete:rm:*' fzf-preview 'eza -a1 --color=always $realpath'
 
 # [[ Zsh standard plugins ]]
 # A few standard plugins for zsh, enabling syntax highlighting (show valid/invalid commands, paths, etc.), autosuggestions (suggest
