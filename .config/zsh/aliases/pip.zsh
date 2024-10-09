a6f6a8e73dccb19ad03a298d614c29343d9b7de4
clementjumel <clement.jumel@gmail.com>
2024-10-09 14:23:37 +0200

    ✨ (zsh): improve pipx & pip aliases

--- .config/zsh/aliases/pip.zsh
+++ .config/zsh/aliases/pip.zsh
@@ -1,10 +1,8 @@
-alias pii='pip install'
-alias piir='pip install -r'
-
-alias pil='pip list'
-
-alias pis='pip show'
-
-alias piup='pip install --upgrade'
-
-alias piui='pip uninstall'
+alias pif='pip freeze'                     # [PI]p [F]reeze: output installed packages in requirements format
+alias pifr='pip freeze > requirements.txt' # [PI]p [F]reeze: output installed packages in a requirements.txt file
+alias pii='pip install'                    # [PI]p [I]nstall: install a package
+alias piir='pip install -r'                # [PI]p [I]nstall [R]equirements: install packages from a file
+alias pil='pip list'                       # [PI]p [L]ist: list installed packages
+alias pis='pip show'                       # [PI]p [S]how: show installed package information
+alias piup='pip install --upgrade'         # [PI]p [UP]grade: upgrade a package
+alias piui='pip uninstall'                 # [PI]p [U]n[I]nstall: uninstall a package
--- .config/zsh/aliases/pipx.zsh
+++ .config/zsh/aliases/pipx.zsh
@@ -1,7 +1,4 @@
-alias pxi='pipx install'
-
-alias pxl='pipx list'
-
-alias pxup='pipx upgrade'
-
-alias pxui='pipx uninstall'
+alias pxi='pipx install'    # [P]ip[X] [I]nstall: install a package
+alias pxl='pipx list'       # [P]ip[X] [L]ist: list installed packages
+alias pxup='pipx upgrade'   # [P]ip[X] [UP]grade: upgrade a package
+alias pxui='pipx uninstall' # [P]ip[X] [U]n[I]nstall: uninstall a package
