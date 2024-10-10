5fc203c42f5e1bf18512a18c27a5328f0e66c08d
clementjumel <clement.jumel@gmail.com>
2024-10-10 17:51:38 +0200

    🩹 (zsh): use ls command in lt & lta aliases

--- .config/zsh/aliases/eza.zsh
+++ .config/zsh/aliases/eza.zsh
@@ -1,3 +1,4 @@
-# `-T` is not an option of the builtin `ls` command
-alias lt='eza -T'   # List tree: list files in directory & sub-directories in a tree-like format
-alias lta='eza -Ta' # List tree all: list files in directory & sub-directories in a tree-like format, including hidden files
+# `-T` is not an option of the builtin `ls` command, hence these aliases are only valid when `ls` is aliased to `eza`. The `eza` options
+#   defined in the `ls` alias will be propagated to the aliases below.
+alias lt='ls -T'   # [L]ist [T]ree: list files in directory & its sub-directories in a tree-like format
+alias lta='ls -Ta' # [L]ist [T]ree [A]ll: list files in directory & its sub-directories in a tree-like format, including hidden files
