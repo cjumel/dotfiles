27e5ca7ac9eabc1359e121225316ebaf553c0e87
clementjumel <clement.jumel@gmail.com>
2024-10-10 17:47:36 +0200

    🔧 (eza): customize eza command

--- .config/eza/eza.zsh
+++ .config/eza/eza.zsh
@@ -1,2 +1,2 @@
-# Overide the builtin `ls` with `eza`; this will be propagated to other aliases using `ls`
-alias ls='eza'
+# Overide the builtin `ls` with `eza`; this alias will be propagated to other aliases using `ls`
+alias ls='eza --color=always --icons=always --group-directories-first'
