49ca8fd7529a90de882a1ec714a582e996059fe7
clementjumel <clement.jumel@gmail.com>
2024-11-08 13:53:51 +0100

    ⚡️ (wezterm): improve default theme colors

--- .config/wezterm/theme/default.lua
+++ .config/wezterm/theme/default.lua
@@ -1,5 +1,6 @@
 local config = {}
 
-config.color_scheme = ""
+config.color_scheme = "Dracula" -- Close to default Wezterm theme, with less aggressive colors & better contrast
+config.colors = { background = "#000000" } -- Pure black background color, like in default theme
 
 return config
