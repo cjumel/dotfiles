2e14ffa2bdaefe70da7ce2f3d922859362132f48
clementjumel <clement.jumel@gmail.com>
2024-11-08 16:22:15 +0100

    📝 (fzf): add descriptions for terminal themes

--- .config/zsh/aliases/fzf.zsh
+++ .config/zsh/aliases/fzf.zsh
@@ -1,29 +1,31 @@
-# List all themes available (default one then in alphabetical order)
-TERMINAL_THEMES='default
-catppuccin-mocha
-catppuccin-frappe
-catppuccin-macchiato
-catppuccin-latte
-gruvbox-dark
-gruvbox-light
-kanagawa-dragon
-kanagawa-lotus
-kanagawa-wave
-nord
-onedark
-onelight
-rose-pine-dawn
-rose-pine-main
-rose-pine-moon
-tokyonight-day
-tokyonight-moon
-tokyonight-night
-tokyonight-storm'
+# List all available themes, and their descriptions (default one first, then in alphabetical order of main themes, then from darkest to
+# lightest)
+TERMINAL_THEMES="default                     -- black, lean & simple
+catppuccin-mocha            -- darkest & modern Catppucin theme
+catppuccin-macchiato        -- intermmediatly dark & lean Catppucin theme
+catppuccin-frappe           -- least dark & vintage Catppucin theme
+catppuccin-latte            -- light & modern Catppucin theme
+gruvbox-dark                -- dark & vintage Gruvbox theme
+gruvbox-light               -- light & vintage Gruvbox theme
+kanagawa-dragon             -- darkest & modern Kanagawa theme
+kanagawa-wave               -- least dark & modern Kanagawa theme
+kanagawa-lotus              -- light & modern Kanagawa theme
+nord                        -- blueish & modern dark theme
+onedark                     -- dark & vintage One theme
+onelight                    -- light & vintatge One theme
+rose-pine-main              -- darkest & lean Rose-pine theme
+rose-pine-moon              -- least dark & lean Rose-pine theme
+rose-pine-dawn              -- light & lean Rose-pine theme
+tokyonight-night            -- darkest & modern Tokyonight theme
+tokyonight-moon             -- intermmediatly dark & modern Tokyonight theme
+tokyonight-storm            -- least dark & modern Tokyonight theme
+tokyonight-day              -- light & modern Tokyonight theme"
 
 # Change the terminal theme with fzf by prompting the user for a new theme & creating the relevant symlinks
 function change_theme() {
     # Make the user select a theme using fzf among the available ones
-    selected_theme=$(echo "$TERMINAL_THEMES" | fzf --prompt="Theme > ")
+    selected_theme_and_description=$(echo "$TERMINAL_THEMES" | fzf --prompt="Theme > ")
+    selected_theme=$(echo "$selected_theme_and_description" | head -n1 | awk '{print $1;}')
 
     # Exit if no theme is selected, to avoid creating broken symlinks
     if [[ -z $selected_theme ]]; then
