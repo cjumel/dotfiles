303e05f488db3807d14343a44b28fbfc50e25fce
clementjumel <clement.jumel@gmail.com>
2024-11-14 14:35:34 +0100

    🩹 (zsh): fix and improve pip aliases

--- .config/zsh/aliases/pip.zsh
+++ .config/zsh/aliases/pip.zsh
@@ -1,9 +1,15 @@
 alias pif='pip freeze'                     # [F]reeze: output installed packages in requirements format
 alias pifr='pip freeze > requirements.txt' # [F]reeze [R]equirements.txt: output installed packages in a requirements.txt file
-alias pii='pip install'                    # [I]nstall: install a package
-alias piie='pip isntall -e'                # [I]nstall [E]ditable: install a local package in editable mode
-alias piir='pip install -r'                # [I]nstall [R]equirements: install packages from a requirements file
-alias pil='pip list'                       # [L]ist: list installed packages
-alias pis='pip show'                       # [S]how: show installed package information
-alias piup='pip install --upgrade'         # [U][P]grade: upgrade a package
-alias piui='pip uninstall'                 # [U]n[I]nstall: uninstall a package
+
+alias pii='pip install'                       # [I]nstall: install a package
+alias piie='pip install -e'                   # [I]nstall [E]ditable: install a local package in editable mode
+alias piir='pip install -r'                   # [I]nstall [R]equirements: install packages from a requirements file
+alias piirr='pip install -r requirements.txt' # [I]nstall [R]equirements [R]equirements.txt: install packages from the requirements.txt file
+
+alias pil='pip list' # [L]ist: list installed packages
+
+alias pis='pip show' # [S]how: show installed package information
+
+alias piup='pip install --upgrade' # [U][P]grade: upgrade a package
+
+alias piui='pip uninstall' # [U]n[I]nstall: uninstall a package
