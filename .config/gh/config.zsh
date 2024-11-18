45f75be27f9fd1771e5adf64be72068bb34aee0e
clementjumel <clement.jumel@gmail.com>
2024-11-18 22:44:24 +0100

    🩹 (gh): fix slow terminal startup due to gh copilot

--- .config/gh/config.zsh
+++ .config/gh/config.zsh
@@ -1,4 +1,10 @@
-# Make available two terminal aliases, as well as other more advanced features:
+# By default, GitHub suggests to use the `eval $(gh alias --shell zsh)` command to set up Copilot in Zsh configuration. However, this makes
+# terminal startup a lot (lot) slower, so I prefer to put it in inside a function which I can call manually to enable Copilot. The Copilot
+# setup makes available two terminal aliases (among other things):
 # - `ghce` to ask Copilot to explain something
 # - `ghcs` to ask Copilot to suggest something
-eval "$(gh copilot alias -- zsh)"
+
+function github_copilot_activate() {
+    eval "$(gh copilot alias -- zsh)"
+}
+alias ghca='github_copilot_activate'
