abccaeb7be5c60284ae12bd64391be9233ab7e29
clementjumel <clement.jumel@gmail.com>
2024-11-27 14:20:27 +0100

    ⚡️ (zsh): more concise theme descriptions in `ct` command

--- .config/zsh/theme.zsh
+++ .config/zsh/theme.zsh
@@ -2,26 +2,26 @@
 # Provide features to manage the terminal-level theme.
 
 # List available themes and their descriptions (default one first, then in alphabetical order of main themes, then darkest to lightest)
-TERMINAL_THEMES="default                     -- black, lean & simple
-catppuccin-mocha            -- darkest & modern Catppucin theme
-catppuccin-macchiato        -- intermmediatly dark & lean Catppucin theme
-catppuccin-frappe           -- least dark & vintage Catppucin theme
-catppuccin-latte            -- light & modern Catppucin theme
-gruvbox-dark                -- dark & vintage Gruvbox theme
-gruvbox-light               -- light & vintage Gruvbox theme
-kanagawa-dragon             -- darkest & modern Kanagawa theme
-kanagawa-wave               -- least dark & modern Kanagawa theme
-kanagawa-lotus              -- light & modern Kanagawa theme
-nord                        -- blueish & modern dark theme
-onedark                     -- dark & vintage One theme
-onelight                    -- light & vintatge One theme
-rose-pine-main              -- darkest & lean Rose-pine theme
-rose-pine-moon              -- least dark & lean Rose-pine theme
-rose-pine-dawn              -- light & lean Rose-pine theme
-tokyonight-night            -- darkest & modern Tokyonight theme
-tokyonight-moon             -- intermmediatly dark & modern Tokyonight theme
-tokyonight-storm            -- least dark & modern Tokyonight theme
-tokyonight-day              -- light & modern Tokyonight theme"
+TERMINAL_THEMES="default                     -- default theme, lean and black
+catppuccin-mocha            -- darkest Catppucin theme
+catppuccin-macchiato        -- intermediately dark Catppucin theme
+catppuccin-frappe           -- least dark Catppucin theme
+catppuccin-latte            -- light Catppucin theme
+gruvbox-dark                -- dark Gruvbox theme
+gruvbox-light               -- light Gruvbox theme
+kanagawa-dragon             -- darkest Kanagawa theme
+kanagawa-wave               -- least dark Kanagawa theme
+kanagawa-lotus              -- light Kanagawa theme
+nord                        -- blueish dark theme
+onedark                     -- dark One theme
+onelight                    -- light One theme
+rose-pine-main              -- darkest Rose-pine theme
+rose-pine-moon              -- least dark Rose-pine theme
+rose-pine-dawn              -- light Rose-pine theme
+tokyonight-night            -- darkest Tokyonight theme
+tokyonight-moon             -- intermmediatly dark Tokyonight theme
+tokyonight-storm            -- least dark Tokyonight theme
+tokyonight-day              -- light Tokyonight theme"
 
 # Change the terminal theme with fzf by prompting the user for a new theme & creating the relevant symlinks
 function change_theme() {
