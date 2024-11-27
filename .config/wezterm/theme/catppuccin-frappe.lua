74b137b07468366580cb2ad4d1c34ef507056d05
clementjumel <clement.jumel@gmail.com>
2024-11-27 17:03:43 +0100

    🩹 (wezterm): make all catppuccin & tokyonight themes transparent

--- .config/wezterm/theme/catppuccin-frappe.lua
+++ .config/wezterm/theme/catppuccin-frappe.lua
@@ -1,7 +1,7 @@
 local config = {}
 
 config.color_scheme = "Catppuccin Frappe"
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.9
 config.harfbuzz_features = {} -- Use ligatures
 
 return config
--- .config/wezterm/theme/tokyonight-storm.lua
+++ .config/wezterm/theme/tokyonight-storm.lua
@@ -1,7 +1,7 @@
 local config = {}
 
 config.color_scheme = "Tokyo Night Storm"
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.9
 config.harfbuzz_features = {} -- Use ligatures
 
 return config
