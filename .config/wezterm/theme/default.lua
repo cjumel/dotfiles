28beed655af245a4bf14fa52372ae54f577540bc
clementjumel <clement.jumel@gmail.com>
2025-01-06 18:56:32 +0100

    🩹 (wezterm): use NvimDark color scheme in default theme

--- .config/wezterm/theme/default.lua
+++ .config/wezterm/theme/default.lua
@@ -1,6 +1,5 @@
 local config = {}
 
-config.color_scheme = "Dracula" -- Close to default Wezterm theme, with less aggressive colors & better contrast
-config.colors = { background = "#000000" } -- Pure black background color, like in default theme
+config.color_scheme = "NvimDark"
 
 return config
