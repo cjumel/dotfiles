6f0f5f8710a320f53ada3bc0e36e58cc824d5c8d
clementjumel <clement.jumel@gmail.com>
2025-01-13 16:39:16 +0100

    ✨ New ashen theme

---
+++ .config/wezterm/theme/ashen.lua
@@ -0,0 +1,22 @@
+local config = {}
+
+-- Colors taken from https://github.com/ficcdaf/ashen.nvim/blob/main/extras/wezterm/ashen.lua
+config.colors = {
+  foreground = "#f5f5f5",
+  background = "#121212",
+
+  cursor_bg = "#f5f5f5",
+  cursor_fg = "#121212",
+  cursor_border = "#f5f5f5",
+
+  selection_fg = "#f5f5f5",
+  selection_bg = "#121212",
+
+  scrollbar_thumb = "#121212",
+  split = "#121212",
+
+  ansi = { "#121212", "#B14242", "#D87C4A", "#E49A44", "#4A8B8B", "#a7a7a7", "#b4b4b4", "#d5d5d5" },
+  brights = { "#949494", "#B14242", "#D87C4A", "#E49A44", "#4A8B8B", "#a7a7a7", "#b4b4b4", "#d5d5d5" },
+}
+
+return config
