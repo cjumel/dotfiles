c66e1ade687e38ea0afe4a948bf3fc8701a155dc
clementjumel <clement.jumel@gmail.com>
2025-01-13 16:33:05 +0100

    🩹 (zsh): move brew aliases under "br" prefix

--- .config/zsh/aliases/brew.zsh
+++ .config/zsh/aliases/brew.zsh
@@ -1,29 +1,31 @@
-alias bb='brew bundle'       # [B]undle: base command for Brewfile management
-alias bbd='brew bundle dump' # [B]undle [D]ump: write the installed formulae & casks in a Brewfile
+# [B][R]ew: the missing package manager for MacOS
 
-alias bc='brew cleanup' # [C]leanup: remove old versions of installed formulae & casks
+alias brb='brew bundle'       # [B]undle: base command for Brewfile management
+alias brbd='brew bundle dump' # [B]undle [D]ump: write the installed formulae & casks in a Brewfile
 
-alias bi='brew install'         # [I]nstall: install a formula or cask
-alias bic='brew install --cask' # [I]nstall [C]ask: install a cask
+alias brc='brew cleanup' # [C]leanup: remove old versions of installed formulae & casks
 
-alias bif='brew info' # [I]n[F]o: show information about a formula or cask
+alias bri='brew install'         # [I]nstall: install a formula or cask
+alias bric='brew install --cask' # [I]nstall [C]ask: install a cask
 
-alias bl='brew list'            # [L]ist: list installed formulae & casks
-alias blc='brew list --cask'    # [L]ist [C]ask: list installed casks
-alias blf='brew list --formula' # [L]ist [F]ormula: list installed formulae
+alias brif='brew info' # [I]n[F]o: show information about a formula or cask
 
-alias blv='brew leaves' # [L]ea[V]es: list installed formulae which are not the dependency of another one
+alias brl='brew list'            # [L]ist: list installed formulae & casks
+alias brlc='brew list --cask'    # [L]ist [C]ask: list installed casks
+alias brlf='brew list --formula' # [L]ist [F]ormula: list installed formulae
 
-alias bo='brew outdated'           # [O]utdated: list outdated formulae & casks
-alias bog='brew outdated --greedy' # [O]utdated [G]reedy: list outdated formulae & casks, including casks with auto-updates or version "latest"
+alias brlv='brew leaves' # [L]ea[V]es: list installed formulae which are not the dependency of another one
 
-alias bp='brew pin' # [P]in: prevent a formula or cask from being upgraded
+alias bro='brew outdated'           # [O]utdated: list outdated formulae & casks
+alias brog='brew outdated --greedy' # [O]utdated [G]reedy: list outdated formulae & casks, including casks with auto-updates or version "latest"
 
-alias bud='brew update' # [U]p[D]ate: update Homebrew itself
+alias brp='brew pin' # [P]in: prevent a formula or cask from being upgraded
 
-alias bug='brew upgrade'           # [U]p[G]rade: update a target formula or cask or all of them
-alias bugg='brew upgrade --greedy' # [U]p[G]rade [G]reedy: update a target formula or cask or all of them, including casks with auto-updates or version "latest"
+alias brud='brew update' # [U]p[D]ate: update Homebrew itself
 
-alias bui='brew uninstall' # [U]n[I]nstall: uninstall a formula or cask
+alias brug='brew upgrade'           # [U]p[G]rade: update a target formula or cask or all of them
+alias brugg='brew upgrade --greedy' # [U]p[G]rade [G]reedy: update a target formula or cask or all of them, including casks with auto-updates or version "latest"
 
-alias bup='brew unpin' # [U]n[P]in: allow a formula or cask to be upgraded
+alias brui='brew uninstall' # [U]n[I]nstall: uninstall a formula or cask
+
+alias brup='brew unpin' # [U]n[P]in: allow a formula or cask to be upgraded
