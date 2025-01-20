2ab688fd9cf8ec3e14ce7a2f1a80453437efae0c
clementjumel <clement.jumel@gmail.com>
2025-01-20 13:51:39 +0100

    ⏪️ (zsh): re-use px prefix for pipx aliases

--- .config/zsh/aliases/pipx.zsh
+++ .config/zsh/aliases/pipx.zsh
@@ -1,6 +1,6 @@
-# [P][I]p[X]: install and manage Python packages in isolated environments
+# [P]ip[X]: install and manage Python packages in isolated environments
 
-alias pixi='pipx install'    # [I]nstall: install a package
-alias pixl='pipx list'       # [L]ist: list installed packages
-alias pixup='pipx upgrade'   # [U][P]grade: upgrade a package
-alias pixui='pipx uninstall' # [U]n[I]nstall: uninstall a package
+alias pxi='pipx install'    # [I]nstall: install a package
+alias pxil='pipx list'      # [L]ist: list installed packages
+alias pxup='pipx upgrade'   # [U][P]grade: upgrade a package
+alias pxui='pipx uninstall' # [U]n[I]nstall: uninstall a package
