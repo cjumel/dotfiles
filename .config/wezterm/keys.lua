843b5284116fbae4a970f8d451c8de6c7d5abc65
cjumel <clement.jumel@gmail.com>
2025-01-21 18:59:51 +0100

    ✨ (wezterm): support <S-CR> & <C-CR> keymaps, change <C-BS>

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -1,5 +1,16 @@
 -- This page has been initialized with the output of `wezterm show-keys --lua`
 
+-- NOTE: the final list of complex key combinations which are supported in my Wezterm configuration but differ from the
+-- physical keyboard equivalent is:
+--  - <C-BS> on my physical keyboard corresponds to <C-w>
+--  - <C-^> on my physical keyboard corresponds to <C-[>
+--  - <C-$> on my physical keyboard corresponds to <C-]>
+--  - <C-m> on my physical keyboard corresponds to <CR>
+--  - <C-ù> on my physical keyboard corresponds to <BS>/<C-?>
+--  - <C-`> on my physical keyboard corresponds to <C-\>
+--  - <C-,> on my physical keyboard corresponds to <C-^>
+--  - <C-;> on my physical keyboard corresponds to <C-_>
+
 local wezterm = require("wezterm")
 
 return {
@@ -37,40 +48,41 @@ return {
   { key = "b", mods = "CMD", action = wezterm.action.EmitEvent("increase-blur") },
   { key = "b", mods = "CMD|SHIFT", action = wezterm.action.EmitEvent("decrease-blur") },
 
-  -- [[ Fix control combinations with special keys ]]
-  -- Enable some control keymaps with special characters by sending the right string given an input sequence
-  -- This is inspired by https://github.com/wez/wezterm/issues/3180, & for terminal codes supported by Neovim see
-  -- https://www.reddit.com/r/neovim/comments/okbag3/how_can_i_remap_ctrl_backspace_to_delete_a_word/
-  -- Some control keymaps with special characters are already working:
-  --  - <C-^> on my keyboard corresponds to <C-[> (escape)
-  --  - <C-$> on my keyboard corresponds to <C-]>
-  --  - <C-`> on my keyboard corresponds to <C-\>
+  -- [[ Complex key combinations ]]
+  -- Enable the use of <S-CR> and <C-CR> in tmux sessions
+  -- Note that in the codes sent by the keymaps, the leading "^[" is actually obtained by entering, in Neovim's insert
+  -- mode, <C-v> and <Esc> to obtain an escape sequence, and the rest of the codes are the actual keycodes
+  -- To make sure this works, in Neovim's insert mode, type <C-v> and then <S-CR> or <C-CR> and you should see the
+  -- corresponding mappings displayed
+  -- Sources:
+  --  - https://stackoverflow.com/questions/16359878/how-to-map-shift-enter
+  --  - https://stackoverflow.com/questions/78053422/how-can-i-get-shift-enter-escape-sequence-to-work-in-a-nested-tmux-session
+  { key = "Return", mods = "SHIFT", action = wezterm.action({ SendString = "[13;2u" }) },
+  { key = "Return", mods = "CTRL", action = wezterm.action({ SendString = "[13;5u" }) },
+
+  -- Remap complex key combinations which has no or little use, to another complex key combinations which would have
+  -- been previously impossible or difficulat to type
+  -- Sources:
+  --  - https://github.com/wez/wezterm/issues/3180,
+  --  - https://www.reddit.com/r/neovim/comments/okbag3/how_can_i_remap_ctrl_backspace_to_delete_a_word/
   {
-    key = "phys:Quote", -- "ù" key on my keyboard
+    key = "Backspace",
     mods = "CTRL",
-    action = wezterm.action({ SendString = "\x7f" }), -- <C-?> (delete)
+    action = wezterm.action({ SendKey = { key = "w", mods = "CTRL" } }),
   },
   {
-    key = "phys:M", -- "," key on my keyboard
+    key = "phys:Quote", -- "ù" on my physical keyboard
+    mods = "CTRL",
+    action = wezterm.action({ SendString = "\x7f" }), -- <BS>/<C-?>
+  },
+  {
+    key = "phys:M", -- "," on my physical keyboard
     mods = "CTRL",
     action = wezterm.action({ SendString = "\x1e" }), -- <C-^>
   },
   {
-    key = "phys:Comma", -- ";" key on my keyboard
+    key = "phys:Comma", -- ";" on my physical keyboard
     mods = "CTRL",
     action = wezterm.action({ SendString = "\x1f" }), -- <C-_>
-  },
-
-  -- [[ New control keymaps ]]
-  -- Map undefined control keymaps to the corresponding (but usable) alt keymaps
-  {
-    key = "Return",
-    mods = "CTRL",
-    action = wezterm.action({ SendKey = { key = "Return", mods = "ALT" } }),
-  },
-  {
-    key = "Backspace",
-    mods = "CTRL",
-    action = wezterm.action({ SendKey = { key = "Backspace", mods = "ALT" } }),
   },
 }
