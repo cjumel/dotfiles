be03204108c1bbd78a51fd7f751cfbc38d88fee4
cjumel <clement.jumel@gmail.com>
2025-03-23 19:24:39 +0100

    🔥 (zsh): clean nvim aliases

--- .config/zsh/aliases/nvim.zsh
+++ .config/zsh/aliases/nvim.zsh
@@ -21,9 +21,6 @@ function nvim_contextual() {
     fi
 }
 
-# Override the default `vi` command
-alias vi='nvim_contextual'              # Open Neovim with contextual setup
-alias vic='nvim --clean'                # [C]lean: open Neovim without any custom configuration
-alias vil='NVIM_LIGHT_MODE=true nvim'   # [L]ight: open Neovim in light mode
-alias vis='nvim'                        # [S]kip: open Neovim without contextual setup
-alias vix='rm -rf ~/.local/share/nvim/' # Discard: delete Neovim local files (not configuration files); useful to re-install Neovim from scratch
+alias vi='nvim_contextual' # Open Neovim with contextual setup
+alias vic='nvim --clean'   # [C]lean: open Neovim without any custom configuration
+alias vis='nvim'           # [S]kip: open Neovim without contextual setup
