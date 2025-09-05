09976437bd412c59fdcf21ee9689e2cd87685331
cjumel <clement.jumel@gmail.com>
2025-09-05 18:51:59 +0200

    ✨ (bat): also override head with bat

--- .config/bat/bat.zsh
+++ .config/bat/bat.zsh
@@ -1,5 +1,5 @@
-# Override the builtin `cat` with `bat`
 alias cat='bat'
+alias head='bat --line-range :10'
 
 # Use `bat` to colorize the `man` pages
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
