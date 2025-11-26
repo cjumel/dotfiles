15ea8d7502461ac1250f1d2154285e237325f633
cjumel <clement.jumel@gmail.com>
2025-11-26 09:50:19 +0100

    🩹 (karabiner): use both sides of mod keys

--- .config/wezterm/options.lua
+++ .config/wezterm/options.lua
@@ -18,4 +18,8 @@ config.font_size = 12.0
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- Remove ligatures
 config.adjust_window_size_when_changing_font_size = false
 
+-- [[ Keys ]]
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
+
 return config
