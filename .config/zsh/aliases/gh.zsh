732ff4604bb9ce52acfe611ccfb110753d221fa5
cjumel <clement.jumel@gmail.com>
2025-11-26 10:52:59 +0100

    ✨ (zsh): update aliases

--- .config/zsh/aliases/gh.zsh
+++ .config/zsh/aliases/gh.zsh
@@ -3,5 +3,6 @@
 alias ghi='gh issue'         # [G]it[H]ub [I]ssue: manage issues
 alias ghic='gh issue create' # [G]it[H]ub [I]ssue [C]reate: create a new issue
 
-alias ghp='gh pr'         # [G]it[H]ub [P]R: manage pull requests
-alias ghpc='gh pr create' # [G]it[H]ub [P]R [C]reate: create a new pull request
+alias ghp='gh pr'                 # [G]it[H]ub [P]R: manage pull requests
+alias ghpc='gh pr create'         # [G]it[H]ub [P]R [C]reate: create a new pull request
+alias ghpcb='gh pr create --base' # [G]it[H]ub [P]R [C]reate [B]ase: create a pull request with the specified base branch
