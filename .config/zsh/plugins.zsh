c40ad34c90cf9da4e5d510a73afb405de1e9b577
cjumel <clement.jumel@gmail.com>
2025-12-16 10:36:20 +0100

    🩹 (zsh): disable fzf-tab preview
    Preview is not really useful for paths as they are short, and they are
    useless for command completion and can hide their descriptions.

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,21 +1,11 @@
-# shellcheck disable=SC2016
+# Zinit plugins
 
-zinit light Aloxaf/fzf-tab     # Should be loaded before plugins like `zsh-autosuggestions`
-zstyle ':completion:*' menu no # Disable builtin tab completion menu
+zinit light Aloxaf/fzf-tab # Should be loaded before `zsh-autosuggestions`
+zstyle ':completion:*' menu no
 zstyle ':fzf-tab:*' fzf-flags \
     --height=40% \
     --bind 'ctrl-s:toggle+down' \
-    --bind 'ctrl-g:top' \
-    --bind 'alt-p:toggle-preview'
-zstyle ':fzf-tab:complete:*' fzf-preview '
-if [[ -d $realpath ]]; then
-    eza -a1 --color=always --icons=always --group-directories-first $realpath
-elif [[ -f $realpath ]]; then
-    bat --color=always --line-range=:500 $realpath
-else
-    echo "$realpath"
-fi
-'
+    --bind 'ctrl-g:top'
 
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
